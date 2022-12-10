ef982be2d7705438085ff9b707e18c2aff9447eb
xtexChooser <xtexchooser@duck.com>
2022-12-10 11:25:10 +0800

    test AP

--- pages/api/ap/shared_inbox.ts
+++ pages/api/ap/shared_inbox.ts
@@ -7,7 +7,7 @@ export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    console.log(req)
-    console.log(JSON.stringify(req))
+    console.log(req.body)
+    console.log(JSON.stringify(req.body))
     res.status(200).end()
 }
