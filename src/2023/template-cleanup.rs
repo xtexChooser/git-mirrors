3896ad3b01d88819a66744548c649372d431bad7
xtexChooser <xtexchooser@duck.com>
2023-02-05 14:52:17 +0800

    fix: update formatter

--- src/2023/template-cleanup.rs
+++ src/2023/template-cleanup.rs
@@ -14,6 +14,7 @@ async fn main() -> Result<()> {
     let comment_regex =
         Regex::new("<!--\\s*.*(/doc|categories|文档|跨语言|Document|分类).*\\s*-->")?;
     let noinclude_regex = Regex::new("<\\s*noinclude\\s*>(\\s|\\n|\\t)*<\\s*/noinclude\\s*\\s*>")?;
+    let documentation_trim_regex = Regex::new("<\\s*noinclude\\s*>(\\s|\\n|\\t)*{{(D|d)ocumentation}}(\\s|\\n|\\t)*<\\s*/noinclude\\s*\\s*>")?;
     let includeonly_regex =
         Regex::new("<\\s*includeonly\\s*>(\\s|\\n|\\t)*<\\s*/includeonly\\s*\\s*>")?;
     let mut pages = allpages(&bot, 10, FilterRedirect::Nonredirects);
@@ -24,14 +25,17 @@ async fn main() -> Result<()> {
         let wt = page.wikitext().await?;
         let wt1 = comment_regex.replace_all(wt.as_str(), "").to_owned();
         let wt2 = noinclude_regex.replace_all(&wt1, "").to_owned();
-        let wt3 = includeonly_regex.replace_all(&wt2, "");
-        let out = wt3.to_owned().to_string();
+        let wt3 = includeonly_regex.replace_all(&wt2, "").to_owned();
+        let wt4 = documentation_trim_regex
+            .replace_all(&wt3, "<noinclude>{{Documentation}}</noinclude>")
+            .to_owned();
+        let out = wt4.to_string();
         if wt != out {
             info!("changed {}", page.title());
             for diff in diff::lines(wt.as_str(), out.as_str()) {
                 match diff {
                     diff::Result::Left(l) => println!("-{}", l),
-                    diff::Result::Both(l, _) => println!(" {}", l),
+                    diff::Result::Both(_, _) => (),
                     diff::Result::Right(r) => println!("+{}", r),
                 }
             }
@@ -41,6 +45,8 @@ async fn main() -> Result<()> {
                 .await?;
             info!("uploaded: {:?}", resp);
             counter += 1;
+        } else {
+            info!("good template: {}", page.title());
         }
     }
     info!("formatted {} pages", counter);
