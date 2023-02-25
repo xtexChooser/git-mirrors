8d14a7354ccf30b9890de82de36f222fb59f1e05
xtexChooser <xtexchooser@duck.com>
2023-02-25 10:24:48 +0800

    feat: cert list

--- spica-signer/src/routes/mod.rs
+++ spica-signer/src/routes/mod.rs
@@ -1,14 +1,20 @@
 use askama::Template;
 use axum::{routing::get, Router};
 
+use crate::cert::get_certs;
+
 pub async fn make_router() -> Router {
     Router::new().route("/", get(index))
 }
 
 async fn index() -> IndexTemplate {
-    IndexTemplate {}
+    IndexTemplate {
+        certs: get_certs().keys().collect(),
+    }
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate {}
+struct IndexTemplate {
+    certs: Vec<&'static String>,
+}
