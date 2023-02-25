c75cd768e7c287ef3be7ec8f37c70b19b8cbe7b9
xtexChooser <xtexchooser@duck.com>
2023-02-25 10:57:38 +0800

    feat: cert files download

--- spica-signer/src/routes/mod.rs
+++ spica-signer/src/routes/mod.rs
@@ -1,11 +1,16 @@
 use askama::Template;
-use axum::{routing::get, Router};
+use askama_axum::IntoResponse;
+use axum::{extract::Path, routing::get, Router};
+use reqwest::{header::CONTENT_TYPE, StatusCode};
 
-use crate::cert::get_certs;
+use crate::cert::{get_cert, get_certs};
 
 pub async fn make_router() -> Router {
-    Router::new().route("/", get(index))
-    .route("/:id", get(index))
+    Router::new()
+        .route("/", get(index))
+        .route("/:id", get(cert_probe))
+        .route("/:id/text.txt", get(cert_text))
+        .route("/:id/cert.crt", get(cert_crt))
 }
 
 async fn index() -> IndexTemplate {
@@ -18,4 +23,28 @@ async fn index() -> IndexTemplate {
 #[template(path = "index.html")]
 struct IndexTemplate {
     certs: Vec<&'static String>,
+}
+
+async fn cert_probe(Path(id): Path<String>) -> StatusCode {
+    match get_cert(&id) {
+        Some(_) => StatusCode::NO_CONTENT,
+        None => StatusCode::NOT_FOUND,
+    }
+}
+
+async fn cert_text(Path(id): Path<String>) -> Result<impl IntoResponse, StatusCode> {
+    match get_cert(&id) {
+        Some(cert) => Ok(cert.text.as_str()),
+        None => Err(StatusCode::NOT_FOUND),
+    }
+}
+
+async fn cert_crt(Path(id): Path<String>) -> Result<impl IntoResponse, StatusCode> {
+    match get_cert(&id) {
+        Some(cert) => Ok((
+            [(CONTENT_TYPE, "application/x-x509-ca-cert")],
+            cert.cert_pem.as_str(),
+        )),
+        None => Err(StatusCode::NOT_FOUND),
+    }
 }
