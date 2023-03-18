9423619ead348a0058a9d60edf2fcf2a57e5454e
xtexChooser <xtexchooser@duck.com>
2023-03-18 20:19:46 +0800

    chore: cargo init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let subscriber = tracing_subscriber::fmt().json().finish();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    Ok(())
+}
