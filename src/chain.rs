679da5073e429fe0badafac5529ea9f80d6461c0
xtexChooser <xtexchooser@duck.com>
2023-03-24 22:38:17 +0800

    feat: chainfile flags

--- src/chain.rs
+++ src/chain.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone, Hash)]
@@ -7,9 +7,28 @@ pub struct Chain(Vec<Vec<String>>);
 
 pub fn parse_chain(text: &str) -> Result<Chain> {
     let mut lines = vec![];
-    for line in text.lines() {
-        let mut line = line;
-        let parts = line.split('.').map(String::from).collect();
+    for mut line in text.lines() {
+        if line.starts_with('#') {
+            continue;
+        }
+        let mut dns_flag = false;
+        while line.starts_with('\\') {
+            if line.starts_with("\\asis ") {
+                line = &line[6..];
+                break;
+            } else if line.starts_with("\\dns ") {
+                dns_flag = true;
+                if !line.ends_with('.') {
+                    bail!("DNS lines in chainfiles must end with a '.' suffix")
+                }
+                line = &line[5..line.len() - 1];
+            }
+        }
+        let parts = if dns_flag {
+            line.split('.').map(String::from).collect()
+        } else {
+            vec![line.to_owned()]
+        };
         lines.push(parts);
     }
     Ok(Chain(lines))
