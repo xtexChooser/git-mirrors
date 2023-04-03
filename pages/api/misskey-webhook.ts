5f3a9229f3b1a7a091946a295d7677bc3bd2846c
xtexChooser <xtexchooser@duck.com>
2023-04-03 14:02:44 +0800

    fix: parse json

--- pages/api/misskey-webhook.ts
+++ pages/api/misskey-webhook.ts
@@ -8,28 +8,44 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<string>
 ) {
-    const body = req.body;
-    if (req.headers['x-misskey-hook-secret'] != env['XTEX_HOME_MISSKEY_SECRET']) {
-        throw 'invalid secret'
+    let body = req.body;
+    if (
+        req.headers["x-misskey-hook-secret"] != env["XTEX_HOME_MISSKEY_SECRET"]
+    ) {
+        throw "invalid secret";
     }
-    console.log('secret check passed')
+    console.log("secret check passed");
+    console.log("body: " + body);
+    if (typeof body == "string") {
+        console.log("parsing json");
+        body = JSON.parse(body);
+    }
 
-    const bot = new TelegramBot(env['XTEX_HOME_MISSKEY_TG_TOKEN'] as string, {});
-    console.log('type: ' + body['type'])
-    switch (body['type'] as string) {
-        case 'note': {
-            const note = body['body']['note'] as any;
-            console.log('note: ' + body['note'])
-            if (body['userId'] == '8pjo5pvnqn' && note['visibility'] == 'public') {
+    const bot = new TelegramBot(
+        env["XTEX_HOME_MISSKEY_TG_TOKEN"] as string,
+        {}
+    );
+    console.log("type: " + body["type"]);
+    switch (body["type"] as string) {
+        case "note": {
+            const note = body["body"]["note"] as any;
+            console.log("note: " + body["note"]);
+            if (
+                body["userId"] == "8pjo5pvnqn" &&
+                note["visibility"] == "public"
+            ) {
                 await sendToNtfy({
-                    topic: 'xtex-logs',
-                    message: `Forwarding Misskey note to TG: ${note['id']}`
+                    topic: "xtex-logs",
+                    message: `Forwarding Misskey note to TG: ${note["id"]}`,
                 });
-                const text = note['renote']?.['text'] || note['text'];
-                await bot.sendMessage('-1001657723727', `${text}\n(src: https://neko.ci/notes/${note['id']})`);
+                const text = note["renote"]?.["text"] || note["text"];
+                await bot.sendMessage(
+                    "-1001657723727",
+                    `${text}\n(src: https://neko.ci/notes/${note["id"]})`
+                );
             }
         }
     }
 
-    res.status(200).send('xtex-home: done');
+    res.status(200).send("xtex-home: done");
 }
