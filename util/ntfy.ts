697168480273290457c369dcfbd24e26766551fa
xtexChooser <xtexchooser@duck.com>
2023-04-03 14:02:52 +0800

    style: format

--- util/ntfy.ts
+++ util/ntfy.ts
@@ -1,14 +1,14 @@
 import { Config, NtfyClient } from "ntfy";
 import { env } from "process";
 
-const ntfy = new NtfyClient('https://ntfy.xvnet.eu.org/');
+const ntfy = new NtfyClient("https://ntfy.xvnet.eu.org/");
 
 export default ntfy;
 
 export async function sendToNtfy(config: Config) {
     config.authorization = {
-        username: '',
-        password: env['XTEX_HOME_NTFY_TOKEN']!
+        username: "",
+        password: env["XTEX_HOME_NTFY_TOKEN"]!,
     };
-    await ntfy.publish(config)
+    await ntfy.publish(config);
 }
