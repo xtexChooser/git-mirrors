ff15fa2b1bdfc79b87d2a89b2f27a7315e73e917
xtexChooser <xtexchooser@duck.com>
2023-04-15 20:22:25 +0800

    feat: start script

--- Containerfile
+++ Containerfile
@@ -2,6 +2,7 @@ FROM docker.io/library/alpine AS builder
 ARG VERSION=2.6.4
 ARG TAG=OPENLDAP_REL_ENG_2_6_4
 
+EXPOSE 389
 LABEL name="openldap" \
     summary="OpenLDAP slapd" \
     maintainer="Xensor V Network <containers@xvnet.eu.org>"
@@ -14,17 +15,24 @@ RUN mkdir -p /dist
 
 RUN apk add gcc make binutils musl musl-dev groff
 
-RUN ./configure --prefix=/dist --localstatedir=/var --sysconfdir=/etc\
+RUN ./configure --prefix=/dist --localstatedir=/var --sysconfdir=/etc \
     --enable-syslog --enable-ipv6 --with-tls --enable-local 
 RUN make depend
 RUN make -j8
 RUN make install
 
 FROM docker.io/library/alpine
-#RUN apk add libssh ncurses readline
+WORKDIR /
+RUN apk add bash
+
 COPY --from=builder /dist /usr
 COPY --from=builder /etc/openldap /etc/openldap
+
+ADD start.sh /usr/lib/openldap-oci/start.sh
+RUN chmod +x /usr/lib/openldap-oci/start.sh
+COPY --from=builder /etc/openldap/schema /usr/lib/openldap-oci/builtin-schema
+
 RUN mkdir -p /var/run/slapd/ /var/lib/
 RUN ln -s /usr/libexec/slapd /usr/sbin/slapd
-WORKDIR /
-ENTRYPOINT [ "/libexec/slapd", "-F", "/etc/openldap/slapd.d", "-h", "ldap:/// ldaps:/// ldapi:///" ]
+
+ENTRYPOINT [ "/usr/lib/openldap-oci/start.sh" ]
---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/usr/bash
+
+SLAPD_D_PATH=${SLAPD_D_PATH:-/etc/openldap/slapd.d}
+SLAPD_CONF_PATH=${SLAPD_CONF_PATH:-/etc/openldap/slapd.conf}
+SLAPD_LDIF_PATH=${SLAPD_LDIF_PATH:-/etc/openldap/slapd.ldif}
+
+SLAPD_LISTEN=${SLAPD_LISTEN:-ldap:/// ldaps:/// ldapi:///}
+SLAPD_OPTS=${SLAPD_OPTS:-}
+
+if [[ "x$OLO_NO_DEFAULT_F" != "xtrue" ]]; then
+    echo adding default configuration paths
+    SLAPD_OPTS=$SLAPD_OPTS "-F" "$SLAPD_D_PATH" "-f" "$SLAPD_CONF_PATH"
+    if [[ ! -e "$SLAPD_D_PATH" ]]; then
+        mkdir -v -p "$SLAPD_D_PATH"
+    fi
+fi
+
+if [[ "x$OLO_NO_DEFAULT_H" != "xtrue" ]]; then
+    echo adding default listen addrs
+    SLAPD_OPTS=$SLAPD_OPTS "-h" "$SLAPD_LISTEN"
+fi
+
+if [[ "x$OLO_NO_LN_BUILTIN_SCHEMA" != "xtrue" ]]; then
+    echo copying builtin schemas
+    cp -R -s -n "$(readlink -e "${OLO_BUILTIN_SCHEMA_PATH:-/usr/lib/openldap-oci/builtin-schema}")" "$(readlink -m "${OLO_SCHEMA_LN_DST:-/etc/openldap/schema}")"
+fi
+
+if [[ "x$OLO_NO_IMPORT_SLAPD_LDIF" != "xtrue" ]]; then
+    echo importing slapd.ldif
+    /usr/sbin/slapadd -n 0 -c -F "$SLAPD_D_PATH" -l "$SLAPD_LDIF_PATH"
+fi
+
+# shellcheck disable=SC2086
+echo executing slapd with $SLAPD_OPTS
+# shellcheck disable=SC2086
+exec "/usr/bin/slapd" $SLAPD_OPTS
