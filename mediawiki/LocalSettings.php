8c3e8cf14ec663485108d0310eca541a11c0e812
xtexChooser <xtexchooser@duck.com>
2023-04-29 07:03:04 +0800

    feat: mw short url

--- mediawiki/LocalSettings.php
+++ mediawiki/LocalSettings.php
@@ -6,12 +6,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgSitename = "XV-NET Wiki";
 $wgMetaNamespace = "XV-NET_Wiki";
 
-## The URL base path to the directory containing the wiki;
-## defaults for all runtime URL paths are based off of this.
-## For more information on customizing the URLs
-## (like /w/index.php/Page_title to /wiki/Page_title) please see:
-## https://www.mediawiki.org/wiki/Manual:Short_URL
 $wgScriptPath = "";
+$wgArticlePath = "/wiki/$1";
+$wgUsePathInfo = true;
 
 ## The protocol and server name to use in fully-qualified URLs
 $wgServer = "https://w.xvnet.eu.org";
