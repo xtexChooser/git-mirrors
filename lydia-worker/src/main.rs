834378596a8d00da43ea57c04324143a3ad34b4e
xtexChooser <xtexchooser@duck.com>
2023-05-21 07:11:49 +0800

    feat: tracing logging

--- lydia-worker/src/main.rs
+++ lydia-worker/src/main.rs
@@ -1,7 +1,18 @@
 use anyhow::Result;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    // init logging
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .json()
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    info!("lydia worker starting");
+
     Ok(())
 }
