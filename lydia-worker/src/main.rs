e2cdc02f1f964c4927a1055770803f3d19c9a316
xtexChooser <xtexchooser@duck.com>
2023-05-27 11:14:55 +0800

    update

--- lydia-worker/src/main.rs
+++ lydia-worker/src/main.rs
@@ -3,30 +3,49 @@ use env::Env;
 use secrets::Secrets;
 use serde::{Deserialize, Serialize};
 use tracing::{info, Level};
-use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::{fmt::writer::MakeWriterExt, FmtSubscriber};
 
 pub mod env;
 pub mod secrets;
 pub mod tf;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Bot {
     pub env: Env,
     pub secrets: Secrets,
 }
 
+impl Bot {
+    pub async fn new() -> Result<Bot> {
+        Ok(Bot {
+            env: env::detect_env()?,
+            secrets: Secrets::new()?,
+        })
+    }
+
+    pub async fn run(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // init logging
+    let file_appender = tracing_appender::rolling::daily("../logs", "worker.log");
+    let (non_blocking_file, _guard) = tracing_appender::non_blocking(file_appender);
+
     let subscriber = FmtSubscriber::builder()
         .with_max_level(Level::TRACE)
+        .with_writer(std::io::stdout.and(non_blocking_file))
         .json()
         .finish();
-
     tracing::subscriber::set_global_default(subscriber)?;
 
-    info!("lydia worker starting");
-    env::detect_env()?;
+    // construct bot
+    info!("lydia-worker started");
+    let mut bot = Bot::new().await?;
 
-    Ok(())
+    // run bot
+    info!("run bot");
+    bot.run().await
 }
--- lydia-worker/src/secrets.rs
+++ lydia-worker/src/secrets.rs
@@ -1,7 +1,51 @@
+use std::{env, fs, path::PathBuf};
+
+use anyhow::{bail, Result};
 use serde::{Deserialize, Serialize};
+use tracing::info;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Clone, Hash)]
-pub struct Secrets {}
+#[derive(PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Clone, Hash)]
+pub struct Secrets {
+    pub wmf: Wmf,
+    pub dc: Dc,
+}
 
-unsafe impl Sync for Secrets {}
-unsafe impl Send for Secrets {}
+#[derive(PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Clone, Hash)]
+pub struct Wmf {
+    pub user: String,
+    pub passwd: String,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Clone, Hash)]
+pub struct Dc {
+    pub url: String,
+}
+
+impl Secrets {
+    pub fn new() -> Result<Secrets> {
+        let path = Self::find_file()?;
+        info!(
+            path = format_args!("{}", path.display()),
+            "located secrets.json"
+        );
+        let str = fs::read_to_string(path)?;
+        Ok(serde_json::from_str(&str)?)
+    }
+
+    fn find_file() -> Result<PathBuf> {
+        if let Ok(path) = env::var("LYDIA_SECRETS_FILE") {
+            let path = PathBuf::from(path);
+            assert!(path.exists());
+            return Ok(path);
+        }
+        let path = PathBuf::from("secrets.json");
+        if path.exists() {
+            return Ok(path);
+        }
+        let path = PathBuf::from("../secrets.json");
+        if path.exists() {
+            return Ok(path);
+        }
+        bail!("secrets.json not found")
+    }
+}
