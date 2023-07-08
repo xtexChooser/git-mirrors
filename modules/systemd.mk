5ed32fb3a3afec84ebf25a25aa83c4ddffa3c22a
xtex <xtexchooser@duck.com>
2023-07-08 10:30:11 +0800

    feat: post-hooks for systemd

--- modules/systemd.mk
+++ modules/systemd.mk
@@ -1,12 +1,13 @@
-SYSTEMD_UNIT_VARS=V_TARGET_NAME V_UNIT V_ENABLED V_DISABLED V_RUNNING V_STOPPED V_USER V_SYSTEMCTL V_POST V_DEPS
 SYSTEMCTL ?= systemctl
 SYSTEMCTL_USER ?= $(SYSTEMCTL) --user
 
+SYSTEMD_UNIT_VARS=V_TARGET_NAME V_UNIT V_ENABLED V_DISABLED V_RUNNING V_STOPPED V_USER V_SYSTEMCTL V_POST V_DEPS
 define systemd-unit0
 $(eval V_TARGET_NAME?=systemd-$(V_UNIT))
 $(eval V_SYSTEMCTL ?= $(if $(V_USER),$(SYSTEMCTL_USER),$(SYSTEMCTL)))
 $(if $(V_ENABLED),$(if $(V_DISABLED),$(error Both V_ENABLED and V_DISABLED is defined for $(V_UNIT))))
 $(if $(V_RUNNING),$(if $(V_STOPPED),$(error Both V_RUNNING and V_STOPPED is defined for $(V_UNIT))))
+$(if $(findstring .,$(V_UNIT)),,$(error V_UNIT $(V_UNIT) should include the suffix, e.g. .service and .timer))
 
 $(call mktrace,Define systemd unit target: $(V_UNIT))
 $(call mktrace-vars,$(SYSTEMD_UNIT_VARS))
@@ -16,7 +17,6 @@ $(V_TARGET_NAME): $(V_DEPS)
 	export E_UNIT=$(V_UNIT)
 $(if $(V_ENABLED),
 	if ! $(V_SYSTEMCTL) is-enabled $(V_UNIT) > /dev/null; then
-		$(call log, Enable SD unit $(V_UNIT))
 		$(V_SYSTEMCTL) enable $(V_UNIT)
 		$(call succ, Enabled SD unit $(V_UNIT))
 		$(if $(V_POST),E_EVENT=systemd.enabled $(MAKE) $(MAKE_FLAGS) $(V_POST))
@@ -24,7 +24,6 @@ $(if $(V_ENABLED),
 )
 $(if $(V_DISABLED),
 	if $(V_SYSTEMCTL) is-enabled $(V_UNIT) > /dev/null; then
-		$(call log, Disable SD unit $(V_UNIT))
 		$(V_SYSTEMCTL) enable $(V_UNIT)
 		$(call succ, Disabled SD unit $(V_UNIT))
 		$(if $(V_POST),E_EVENT=systemd.disabled $(MAKE) $(MAKE_FLAGS) $(V_POST))
@@ -32,7 +31,6 @@ $(if $(V_DISABLED),
 )
 $(if $(V_RUNNING),
 	if ! $(V_SYSTEMCTL) is-active $(V_UNIT) > /dev/null; then
-		$(call log, Activate SD unit $(V_UNIT))
 		$(V_SYSTEMCTL) start $(V_UNIT)
 		$(call succ, Started SD unit $(V_UNIT))
 		$(if $(V_POST),E_EVENT=systemd.activated $(MAKE) $(MAKE_FLAGS) $(V_POST))
@@ -40,7 +38,6 @@ $(if $(V_RUNNING),
 )
 $(if $(V_STOPPED),
 	if $(V_SYSTEMCTL) is-active $(V_UNIT) > /dev/null; then
-		$(call log, Deactivate SD unit $(V_UNIT))
 		$(V_SYSTEMCTL) stop $(V_UNIT)
 		$(call succ, Stopped SD unit $(V_UNIT))
 		$(if $(V_POST),E_EVENT=systemd.deactivated $(MAKE) $(MAKE_FLAGS) $(V_POST))
@@ -51,3 +48,17 @@ $(call unset-vars)
 endef
 
 $(call define-func, systemd-unit)
+
+$(call vt-target,systemd-restart systemd-reload systemd-daemon-reload systemd-user-daemon-reload)
+systemd-restart:
+	$(SYSTEMCTL) restart $(E_UNIT)
+	$(call succ, Restarted SD unit $(E_UNIT))
+systemd-reload:
+	$(SYSTEMCTL) reload $(E_UNIT)
+	$(call succ, Reloaded SD unit $(E_UNIT))
+systemd-daemon-reload:
+	$(SYSTEMCTL) daemon-reload
+	$(call succ, Reloaded systemd daemon)
+systemd-user-daemon-reload:
+	$(SYSTEMCTL_USER) daemon-reload
+	$(call succ, Reloaded user systemd daemon)
