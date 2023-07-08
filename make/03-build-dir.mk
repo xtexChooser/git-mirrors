f023812cf0286c8261e0529c7f744236cdfcff0e
xtex <xtexchooser@duck.com>
2023-07-08 14:57:26 +0800

    feat: apply ts file

--- make/03-build-dir.mk
+++ make/03-build-dir.mk
@@ -1,4 +1,17 @@
 $(BUILD_DIR):
-	mkdir -p $(BUILD_DIR)
+	@mkdir -p $@
+	$(call succ, Created $@)
 
 $(call run-on-build,$(BUILD_DIR))
+
+$(APPLY_DIR): $(BUILD_DIR)
+	@mkdir $@
+	$(call succ, Created $@)
+
+$(call run-on-apply,$(APPLY_DIR))
+
+$(APPLY_TIME_FILE): $(APPLY_DIR)
+	@touch $@
+	$(call trace, Updated $@)
+
+$(call run-on-apply,$(APPLY_TIME_FILE))
