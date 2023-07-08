c827cb906430a3078b6141fc84f7292379009120
xtex <xtexchooser@duck.com>
2023-07-08 12:06:04 +0800

    feat: only install with instfile when instfile is present

--- modules/package.mk
+++ modules/package.mk
@@ -12,13 +12,13 @@ $(call apply-target,$(V_TARGET_NAME))
 $(call vt-target,$(V_TARGET_NAME))
 $(V_TARGET_NAME): $(V_DEPS) $(if $(V_REMOVED),,$(V_INST_FILE))
 	export E_MAJOR=pkg E_PKG=$(E_PKG)
-$(if $(V_ENABLED),
+$(if $(V_ENABLED),$(if $(V_INST_FILE),,
 	if ! $(V_PKCON) resolve $(V_PKG) | grep 'installed:' > /dev/null; then
 		$(V_PKCON) install $(V_PKG)
 		$(call succ, Installed package $(V_UNIT))
 		$(call vpost, E_MINOR=installed)
 	fi
-)
+))
 $(if $(V_REMOVED),
 $(if $(V_INST_FILE),
 	if [[ -e $(V_INST_FILE) ]]; then,
