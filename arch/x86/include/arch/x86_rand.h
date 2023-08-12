a2a34d4ec8d12a473c395c95a7e560720be903c2
xtex <xtexchooser@duck.com>
2023-08-12 22:26:14 +0800

    fix(x86/rand): use RDTSC for RNG
    Some processors do not support RDRAND and RDSEED
    
    Signed-off-by: xtex <xtexchooser@duck.com>

--- arch/x86/include/arch/x86_rand.h
+++ arch/x86/include/arch/x86_rand.h
@@ -1,12 +1,9 @@
 #ifndef __ARCH_X86_RAND_HEADER__
 #define __ARCH_X86_RAND_HEADER__ 1
 
-#include <immintrin.h>
-
 u64 x86rand() {
 	u32 low, high;
-	_rdrand32_step(&low);
-	_rdrand32_step(&high);
+	asm volatile("rdtsc" : "=a"(low), "=d"(high));
 	return ((u64)high) << 32 | low;
 }
 
