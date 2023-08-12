e1db945a3e8b453225f0a74e1e1b704ccd869b5c
xtex <xtexchooser@duck.com>
2023-08-12 22:37:30 +0800

    fix(math): flooru and ceilu
    Signed-off-by: xtex <xtexchooser@duck.com>

--- core/include/math.h
+++ core/include/math.h
@@ -4,7 +4,7 @@
 #define min(a, b) ((a) < (b) ? (a) : (b))
 #define max(a, b) ((a) < (b) ? (b) : (a))
 
-#define flooru(v, m) (v - (v % m))
-#define ceilu(v, m) ((v % m > 0) ? floor_unsigned(v + m) : v)
+#define flooru(v, m) ((v) - ((v) % (m)))
+#define ceilu(v, m) (((v) % (m) > 0) ? flooru((v) + (m), (m)) : (v))
 
 #endif
