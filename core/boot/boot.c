53cfccfea7693a8b14263f1845235f06d7aa2a46
xtex <xtexchooser@duck.com>
2023-08-13 10:28:10 +0800

    feat(boot): implement ASLR locator
    Signed-off-by: xtex <xtexchooser@duck.com>

--- core/boot/boot.c
+++ core/boot/boot.c
@@ -45,14 +45,40 @@ void do_core_boot(boot_info_t *bootinfo) {
 	// check_arch_boot_memory_available
 	bootinfo->random = arch_boot_rand();
 	char buf[20];
-	//void *core_start = find_core_boot_mem(bootinfo);
-	itoa(buf, 'x', (unsigned)bootinfo->random);
+	void *core_start = find_core_boot_mem(bootinfo);
+	itoa(buf, 'x', (unsigned)core_start);
 	print(buf);
 	arch_pre_boot(bootinfo);
 }
 
 void *find_core_boot_mem(boot_info_t *bootinfo) {
 	// Find with ASLR
-	//void *load_base = (void *)flooru(bootinfo->random, SZ_4K) + SZ_1M;
-	return NULL;
+	void *load_base = (void *)flooru(bootinfo->random, SZ_4K) + SZ_2M;
+	usize core_size = (usize)(bootinfo->core_end - bootinfo->core_start);
+	while (1) {
+		void *load_end = load_base + core_size;
+		if (load_end < (bootinfo->mem_upper - SZ_1M)) {
+			if (check_arch_boot_memory_available(load_base, load_end)) {
+				boot_reserved_mem_t *memblock = bootinfo->reserved_mem;
+				bool passed = true;
+				while (memblock != NULL) {
+					if (max(memblock->start, load_base) <
+						min(memblock->end, load_end)) {
+						passed = false;
+						break;
+					}
+					memblock = memblock->next;
+				}
+				if (passed)
+					break;
+			}
+			load_base = (void *)ceilu((usize)load_base + SZ_4K, SZ_4K);
+		} else
+			load_base = (void *)flooru((usize)load_base / 2, SZ_4K);
+		if ((usize)load_base <= SZ_2M) {
+			print("boot: ASLR locate failed\n");
+			return NULL;
+		}
+	}
+	return load_base;
 }
