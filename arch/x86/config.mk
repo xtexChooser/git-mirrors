125a848954f506d9cfc8b799d3b1d7661b4bff60
xtex <xtexchooser@duck.com>
2023-08-17 10:27:26 +0800

    fix(x86/build): disable more vector instrument sets
    Signed-off-by: xtex <xtexchooser@duck.com>

--- arch/x86/config.mk
+++ arch/x86/config.mk
@@ -1,5 +1,5 @@
 CLANG_TARGET 	= i386-pc-none-elf
 MUSL_TARGET 	= i386-pc-linux-unknown
 cflags			+= -m32
-cflags-boot		+= -mno-avx
-cflags-core		+= -mno-avx # todo: enable AVX for core
+cflags-boot		+= -mno-avx -mno-avx512f -mno-avx2 -mno-mmx -mno-sse
+cflags-core		+= -mno-avx -mno-avx512f -mno-avx2 -mno-mmx -mno-sse # todo: enable AVX for core
