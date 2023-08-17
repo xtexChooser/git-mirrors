1aab9714347dc18ba3f60303a82b334de5ab3638
xtex <xtexchooser@duck.com>
2023-08-17 10:05:28 +0800

    fix(x86/build): disable AVX
    Signed-off-by: xtex <xtexchooser@duck.com>

--- arch/x86/config.mk
+++ arch/x86/config.mk
@@ -1,5 +1,5 @@
 CLANG_TARGET 	= i386-pc-none-elf
 MUSL_TARGET 	= i386-pc-linux-unknown
 cflags			+= -m32
-cflags-boot		+= -fno-avx
-cflags-core		+= -fno-avx # todo: enable AVX for core
+cflags-boot		+= -mno-avx
+cflags-core		+= -mno-avx # todo: enable AVX for core
