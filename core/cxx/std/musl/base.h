5600f3045139217286947f7450b000827bd08fde
xtex <xtexchooser@duck.com>
2023-08-19 18:06:56 +0800

    feat(cxxstd): add math functions
    Signed-off-by: xtex <xtexchooser@duck.com>

---
+++ core/cxx/std/musl/base.h
@@ -0,0 +1,9 @@
+#define __GNUC_PREREQ(a, b) 0
+#include <types.h>
+#pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Wextra"
+// #pragma clang diagnostic ignored "-Wint-conversion"
+// #pragma clang diagnostic ignored "-Wmacro-redefined"
+// #pragma clang diagnostic ignored "-Wsign-compare"
+#include <stddef.h>
+#include <stdint.h>
