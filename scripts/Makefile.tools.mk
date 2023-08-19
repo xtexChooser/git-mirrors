f7d778ae97d0c16e221d4abed6f7ac70fb351621
xtex <xtexchooser@duck.com>
2023-08-19 22:23:12 +0800

    fix(build): ignore symbolic links
    Formatting leaked to external/
    
    Signed-off-by: xtex <xtexchooser@duck.com>

--- scripts/Makefile.tools.mk
+++ scripts/Makefile.tools.mk
@@ -2,4 +2,4 @@
 
 fmt:
 	$(call action,"FMT  ")
-	clang-format -i $$(find arch/ core/ -type f \( -iname \*.c -o -iname \*.h -o -iname \*.cpp -o -iname \*.hpp \))
+	clang-format -i $$(find -P arch/ core/ -type f \( -iname \*.c -o -iname \*.h -o -iname \*.cpp -o -iname \*.hpp \))
