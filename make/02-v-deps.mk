4fee4deb5971ea59370eaafc2b594c74a1b634fd
xtex <xtexchooser@duck.com>
2023-09-10 17:01:17 +0800

    feat(fs-file): re-template file when vars changed
    Signed-off-by: xtex <xtexchooser@duck.com>

--- make/02-v-deps.mk
+++ make/02-v-deps.mk
@@ -3,3 +3,7 @@ v-deps-var := V_DEPS V_DEP_VARS
 define v-deps
 $(V_DEPS) $(call dep-vars,$(V_DEP_VARS))
 endef
+
+define v-var-dep-files
+$(foreach var,$(V_DEP_VARS),$(saved-var-$(var)))
+endef
--- modules/fs-file.mk
+++ modules/fs-file.mk
@@ -20,7 +20,7 @@ $(V_TARGET_NAME): $(V_PATH)
 $(if $(V_USER)$(V_USER_ID)$(V_GROUP)$(V_GROUP_ID)$(V_ACCESS),$(call vt-target,$(V_PATH)))
 $(V_PATH): $(v-deps) $(V_TPL_DEPS)
 	export E_MAJOR=fs-file E_PATH=$(V_PATH)
-	if [[ ! -e $(V_PATH) $(foreach tpldep,$(V_TPL_DEPS),|| "$(tpldep)" -nt "$(V_PATH)" ) ]]; then
+	if [[ ! -e $(V_PATH) $(foreach tpldep,$(V_TPL_DEPS) $(v-var-dep-files),|| "$(tpldep)" -nt "$(V_PATH)" ) ]]; then
 		$(MKDIR) -p $(dir $(V_PATH))
 		$(if $(V_CREATE),
 		$(MAKE) $(MAKE_FLAGS) E_MAJOR=fs-file-create E_PATH=$(V_PATH) mkfile-$(V_CREATE),
