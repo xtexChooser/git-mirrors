9a8464862876e05183ff97489d18d5b8e4d16326
xtex <xtexchooser@duck.com>
2023-09-10 16:26:36 +0800

    docs: add comment
    Signed-off-by: xtex <xtexchooser@duck.com>

--- make/00-path.mk
+++ make/00-path.mk
@@ -3,5 +3,7 @@ BUILD_DIR ?= $(VENDOR_CODE_DIR)/out
 APPLY_DIR = $(BUILD_DIR)/apply
 STAMPS_DIR = $(APPLY_DIR)/stamps
 STAMP_APPLICATION = $(STAMPS_DIR)/application
+# reference stamp is for usage that need a time in the future
+# a old timestamp as reference can be used to avoid clock skew warning by make
 STAMP_REF = $(STAMPS_DIR)/reference
 VARS_DIR = $(APPLY_DIR)/vars
