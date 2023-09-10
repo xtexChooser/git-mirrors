62c30177d0b241e43f77c5fae32b9813e85cbea2
xtex <xtexchooser@duck.com>
2023-09-10 09:02:12 +0800

    feat: add bash-tpl and mo deps
    Signed-off-by: xtex <xtexchooser@duck.com>

---
+++ external/update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -sSL https://raw.githubusercontent.com/TekWizely/bash-tpl/main/bash-tpl -o bash-tpl
+curl -sSL https://raw.githubusercontent.com/TekWizely/bash-tpl/main/LICENSE -o LICENSE.bash-tpl
+
+curl -sSL https://raw.githubusercontent.com/tests-always-included/mo/master/mo -o mo
+curl -sSL https://raw.githubusercontent.com/tests-always-included/mo/master/LICENSE.md -o LICENSE.mo
