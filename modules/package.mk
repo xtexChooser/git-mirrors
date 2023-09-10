5c4e94132175af4f5fbbb6f10717f1fd74a473db
xtex <xtexchooser@duck.com>
2023-09-10 09:24:05 +0800

    fix: package manager
    Signed-off-by: xtex <xtexchooser@duck.com>

--- modules/package.mk
+++ modules/package.mk
@@ -10,15 +10,15 @@ $(call mktrace-vars,$(PACKAGE_VARS))
 $(call apply-target,$(V_TARGET_NAME))
 $(call vt-target,$(V_TARGET_NAME))
 $(V_TARGET_NAME): $(V_DEPS) $(if $(call is-true,$(V_INSTALLED)),$(V_INST_FILE))
-	export E_MAJOR=pkg E_PKG=$(E_PKG)
-$(if $(call is-true,$(V_INSTALLED)),$(if $(V_INST_FILE),,
+	export E_MAJOR=pkg E_PKG=$(V_PKG)
+$(if $(call is-true,$(V_INSTALLED)),$(if $(call not,$(V_INST_FILE)),
 	if ! $(V_PKCON) resolve $(V_PKG) | grep 'installed:' > /dev/null; then
 		$(V_PKCON) install $(V_PKG)
 		$(call succ, Installed package $(V_UNIT))
 		$(call vpost, E_MINOR=installed)
 	fi
 ))
-$(if $(call is-true,$(V_INSTALLED)),
+$(if $(call is-false,$(V_INSTALLED)),
 $(if $(V_INST_FILE),
 	if [[ -e $(V_INST_FILE) ]]; then,
 	if $(V_PKCON) resolve $(V_PKG) | grep 'installed:' > /dev/null; then
