d6442f1f8472b5e080fd9b17b39998efb1edd80e
xtex <xtexchooser@duck.com>
2023-11-05 15:57:51 +0800

    feat: systemd units
    Signed-off-by: xtex <xtexchooser@duck.com>

--- services/atremis/base.mk
+++ services/atremis/base.mk
@@ -7,3 +7,22 @@ $(call fs-file)
 V_PATH		= /usr/local/bin/tiang
 V_SYMLINK	= $(ATRE_DIR)/services/atremis/bin/tiang
 $(call end)
+
+$(call fs-file)
+V_PATH		= /etc/systemd/system/atre-pull.service
+V_COPY		= $(ATRE_DIR)/services/atremis/systemd/atre-pull.service
+V_POST		= systemd-sudo-daemon-reload
+$(call end)
+
+$(call fs-file)
+V_PATH		= /etc/systemd/system/atre-pull.timer
+V_COPY		= $(ATRE_DIR)/services/atremis/systemd/atre-pull.timer
+V_DEPS		+= /etc/systemd/system/atre-pull.service
+V_POST		= systemd-sudo-daemon-reload
+$(call end)
+
+$(call systemd-unit)
+V_UNIT		= atre-pull.timer
+V_ENABLED	= y
+V_DEPS		= /etc/systemd/system/atre-pull.timer
+$(call end)
