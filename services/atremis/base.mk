233ea2985b81ab97eea7a46135fb510859e7479d
xtex <xtexchooser@duck.com>
2023-11-12 10:27:11 +0800

    feat: set service as linger state
    Signed-off-by: xtex <xtexchooser@duck.com>

--- services/atremis/base.mk
+++ services/atremis/base.mk
@@ -8,21 +8,28 @@ V_PATH		= /usr/local/bin/tiang
 V_SYMLINK	= $(ATRE_DIR)/services/atremis/bin/tiang
 $(call end)
 
+ATRE_SYSTEMD_USER_DIR=/home/service/.config/systemd/user
+
 $(call fs-file)
-V_PATH		= /etc/systemd/system/atre-pull.service
+V_PATH		= $(ATRE_SYSTEMD_USER_DIR)/atre-pull.service
 V_COPY		= $(ATRE_DIR)/services/atremis/systemd/atre-pull.service
-V_POST		= systemd-sudo-daemon-reload
+V_POST		= systemd-user-daemon-reload
 $(call end)
 
 $(call fs-file)
-V_PATH		= /etc/systemd/system/atre-pull.timer
+V_PATH		= $(ATRE_SYSTEMD_USER_DIR)/atre-pull.timer
 V_COPY		= $(ATRE_DIR)/services/atremis/systemd/atre-pull.timer
-V_DEPS		+= /etc/systemd/system/atre-pull.service
-V_POST		= systemd-sudo-daemon-reload
+V_DEPS		+= $(ATRE_SYSTEMD_USER_DIR)/atre-pull.service
+V_POST		= systemd-user-daemon-reload
 $(call end)
 
 $(call systemd-unit)
 V_UNIT		= atre-pull.timer
 V_ENABLED	= y
-V_DEPS		= /etc/systemd/system/atre-pull.timer
+V_DEPS		= $(ATRE_SYSTEMD_USER_DIR)/atre-pull.timer
+$(call end)
+
+$(call loginctl)
+V_USER		= service
+V_LINGER	= y
 $(call end)
