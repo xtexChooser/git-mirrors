43225a5c179a9cff60b2c1c1758c26251edf56a4
xtex <xtexchooser@duck.com>
2023-12-16 22:18:15 +0800

    feat: output

--- minecraft-wiki/zh-sprite-deprecate-tracker/script.js
+++ minecraft-wiki/zh-sprite-deprecate-tracker/script.js
@@ -1,13 +1,22 @@
 mw.loader
 	.using(['mediawiki.util', 'mediawiki.api', 'oojs-ui-windows'])
 	.done(function () {
-		const config = mw.config.get(['wgWikiID', 'wgCategories', 'wgPageName']);
+		const config = mw.config.get([
+			'wgWikiID',
+			'wgCategories',
+			'wgPageName',
+			'wgUserLanguage',
+		]);
 		if (config.wgWikiID !== 'zh_mcwiki')
 			return OO.ui.alert(
 				'minecraft-wiki zh-sprite-deprecate-tracker only works on zh mcwiki!'
 			);
 		window.loadedMCWZHSpriteDeprecateTracker = true;
 		if (!config.wgCategories.includes('使用已弃用Sprite的页面')) return;
+
+		mw.util.addCSS(
+			'.mcwzh-sprite-deprecate-tracker { background: none; padding: 0.5em; }'
+		);
 
 		const api = new mw.Api();
 		api.get({
@@ -16,10 +25,11 @@ mw.loader
 			prop: 'wikitext',
 			formatversion: '2',
 		}).done(function (spriteMod) {
-			const spriteSrc = spriteMod.parse.wikitext
-				.replace(```categories[#categories + 1] = '[[Category:使用已弃用Sprite的页面]]'```,
-					```'[[Category:SPRITE DEPRECATE TRACK/' .. args.data .. '/' .. ( mw.text.trim( tostring( args[1] or '' ) ) ) .. ']]'```);
-			api.get({
+			const spriteSrc = spriteMod.parse.wikitext.replace(
+				"categories[#categories + 1] = '[[Category:使用已弃用Sprite的页面]]'",
+				"categories[#categories + 1] = '[[Category:SPRITE DEPRECATE TRACK/' .. args.data .. '/' .. ( mw.text.trim( tostring( args[1] or '' ) ) ) .. ']]'"
+			);
+			api.post({
 				action: 'parse',
 				page: config.wgPageName,
 				prop: 'categories',
@@ -28,23 +38,39 @@ mw.loader
 				templatesandboxcontentmodel: 'Scribunto',
 				formatversion: 2,
 			}).done(function (data) {
-				console.log(data)
-			});
-		});
+				const catPrefix = 'SPRITE DEPRECATE TRACK';
+				const parsed = data.parse.categories
+					.map((c) => c.category)
+					.filter((c) => c.startsWith(catPrefix))
+					.map((c) => c.substring(catPrefix.length + 1));
+				var deprecated = {};
+				var text = '已弃用精灵图：\n';
+				for (c of parsed) {
+					const dataset = c.substring(0, c.indexOf('/'));
+					const name = c.substring(c.indexOf('/') + 1);
+					if (!(dataset in deprecated)) {
+						deprecated[dataset] = [];
+					}
+					deprecated[dataset].push(name);
+				}
+				for (c in deprecated) {
+					text += `* ${c}\n`;
+					for (n of deprecated[c]) {
+						text += `** ${n}\n`;
+					}
+				}
 
-		api.parse(text, {
-			disablelimitreport: true,
-			wrapoutputclass:
-				'horse-userprofile mw-message-box mw-content-' +
-				($('#contentSub').attr('dir') || 'ltr'),
-			uselang: config.wgUserLanguage,
-		}).done(function (parsedText) {
-			parsedText = parsedText.replace(
-				/\u29FCgroup-([^\u29FC\u29FD]+?)(?:-member)?\u29FD/g,
-				'$1'
-			);
-			mw.hook('wikipage.content').fire(
-				$(parsedText).appendTo('#contentSub')
-			);
+				api.parse(text, {
+					disablelimitreport: true,
+					wrapoutputclass:
+						'mcwzh-sprite-deprecate-tracker mw-message-box mw-content-' +
+						($('#contentSub').attr('dir') || 'ltr'),
+					uselang: config.wgUserLanguage,
+				}).done(function (pt) {
+					mw.hook('wikipage.content').fire(
+						$(pt).appendTo('#bodyContent')
+					);
+				});
+			});
 		});
 	});
