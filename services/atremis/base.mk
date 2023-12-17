4bf7bdeba757a98727d023dbe8073d355599d5fa
xtex <xtexchooser@duck.com>
2023-12-17 13:45:10 +0800

    Install dinit

--- services/atremis/base.mk
+++ services/atremis/base.mk
@@ -1,3 +1,5 @@
+# ========== Atremis Executables ==========
+
 $(call fs-file)
 V_PATH		= /usr/local/bin/atre
 V_SYMLINK	= $(ATRE_DIR)/services/atremis/bin/atre
@@ -7,6 +9,8 @@ $(call fs-file)
 V_PATH		= /usr/local/bin/tiang
 V_SYMLINK	= $(ATRE_DIR)/services/atremis/bin/tiang
 $(call end)
+
+# ========== Atremis Systemd Services ==========
 
 ATRE_SYSTEMD_USER_DIR=/home/service/.config/systemd/user
 
@@ -30,7 +34,16 @@ V_ENABLED	= y
 V_DEPS		= $(ATRE_SYSTEMD_USER_DIR)/atre-pull.timer
 $(call end)
 
+# ========== 'service' User ==========
+
 $(call loginctl)
 V_USER		= service
 V_LINGER	= y
+$(call end)
+
+# ========== dinit ==========
+$(call package)
+V_PKG		= dinit
+V_INSTALLED	= y
+V_INST_FILE	= /usr/bin/dinit
 $(call end)
