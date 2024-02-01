86cae2b7f83e46aa05997204ad3056291b49c859
xtex <xtexchooser@duck.com>
2024-02-01 19:43:16 +0800

    i18n: change language code

--- src/web/i18n.rs
+++ src/web/i18n.rs
@@ -7,7 +7,7 @@ use crate::site;
 
 const LANG_DATA: phf::Map<&str, &str> = phf_map! {
 	"en_us" => include_str!("../../langs/en_us.json"),
-	"zh_cn" => include_str!("../../langs/zh_cn.json"),
+	"zh_cn" => include_str!("../../langs/zh_Hans.json"),
 };
 
 static mut LANG: BTreeMap<(&str, &str), &str> = BTreeMap::new();
