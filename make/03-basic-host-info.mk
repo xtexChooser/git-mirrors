10a67cae6464a9de7d41f89092d52797c53f4804
xtex <xtexchooser@duck.com>
2024-02-16 22:18:36 +0800

    Remove slash suffix from HOME

--- make/03-basic-host-info.mk
+++ make/03-basic-host-info.mk
@@ -2,5 +2,5 @@ HOST_USER = $(USER)
 $(if $(call not,$(HOSTNAME)),$(eval HOSTNAME := $(shell hostname)))
 HOST_KERNEL ?= $(shell uname -a)
 
-HOME := $(HOME)/
-$(if $(call not,$(HOME)),$(eval HOME := $(shell echo $$HOME))/)
+HOME := $(abspath $(HOME))
+$(if $(call not,$(HOME)),$(eval HOME := $$(abspath $$(shell echo $$$$HOME))))
