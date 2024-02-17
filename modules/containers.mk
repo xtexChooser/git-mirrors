97de650aba22885693a2e198f891e5e8c322e0a5
xtex <xtexchooser@duck.com>
2024-02-17 17:04:56 +0800

    containers: Reload dinit service file after update

--- modules/containers.mk
+++ modules/containers.mk
@@ -2,11 +2,12 @@ X_CONTAINER_SERVICE_VARS = V_TARGET_NAME V_SERVICE V_STOPPED V_ARGS V_POST $(v-d
 
 define x-container-service0
 $(eval V_PIDFILE?=/var/run/containers/$(V_SERVICE).pid)
+$(eval V_DEP_VARS+=V_ARGS)
 
 $(call mktrace, Define x-container-service target: $(V_SERVICE))
 $(call mktrace-vars,$(X_CONTAINER_SERVICE_VARS))
 
-$(DINITD_DIR)/$(V_SERVICE): $(VENDOR_MODULES_DIR)/containers.mk
+$(DINITD_DIR)/$(V_SERVICE): $(v-deps) $(VENDOR_MODULES_DIR)/containers.mk
 	@cat >$$@ <<EOF
 	type = bgprocess
 	command = atre apply x-service-$(V_SERVICE)-start
@@ -14,6 +15,8 @@ $(DINITD_DIR)/$(V_SERVICE): $(VENDOR_MODULES_DIR)/containers.mk
 	pid-file = $(V_PIDFILE)
 	restart = true
 	EOF
+	$(DINITCTL) stop $(V_SERVICE)
+	$(DINITCTL) reload $(V_SERVICE)
 
 $(call vt-target,x-service-$(V_SERVICE)-start x-service-$(V_SERVICE)-stop)
 x-service-$(V_SERVICE)-start:
@@ -26,6 +29,7 @@ x-service-$(V_SERVICE)-stop:
 $$(call dinit-service)
 V_SERVICE	= $(V_SERVICE)
 V_RUNNING	= $(call not,$(V_STOPPED))
+V_DEPS		+= $(DINITD_DIR)/$(V_SERVICE)
 $$(call end)
 
 $(call unset-vars)
--- services/caddy/base.mk
+++ services/caddy/base.mk
@@ -1,8 +1,3 @@
-# $(call fs-file)
-# V_PATH		= $(DINITD_DIR)/caddy
-# V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/caddy/dinit-caddy
-# $(call end)
-
 $(call x-container-service)
 V_SERVICE	= caddy
 V_ARGS		+= --cap-add=CAP_NET_BIND_SERVICE
@@ -16,11 +11,3 @@ $(call end)
 # V_PATH		= $(DINITD_DIR)/boot.d/caddy
 # V_SYMLINK	= ../caddy
 # $(call end)
-
-# $(call vt-target, caddy-vcmd-start)
-# caddy-vcmd-start:
-# 	mkdir -p /var/run/caddy
-# 	podman run --name caddy \
-# 		--rm -d --pidfile=/var/run/containers/caddy.pid \
-
-# $(call cmd-target, caddy-vcmd-stop)
