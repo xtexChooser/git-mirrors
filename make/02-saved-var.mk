eb1cd865db9f0f221774a2bb65e9ff061dc96ddf
xtex <xtexchooser@duck.com>
2024-02-17 19:46:36 +0800

    saved-var: Fix unterminated if

--- make/02-saved-var.mk
+++ make/02-saved-var.mk
@@ -12,13 +12,13 @@ $(saved-var-$1):
 
 $(empty)define saved-var-$1-restore
 $$$$(eval -include $(saved-var-$1))
-$(empty)$$$$(if $$$$(call strneq,$$$$(saved_var_$1_value),$$$$($2)),
+$(empty)ifneq ($$$$(saved_var_$1_value),$$$$($2))
 $$$$(file > $$$$(saved-var-$1),saved_var_$1_value=$$$$($2))
-$$$$(call mktrace, Invalidated variable cache $1. Old: $$$$(saved_var_$1_value) New: $$($2))
+$$$$(call mktrace, Invalidated variable cache $1. Old: $$$$(saved_var_$1_value) New: $$$$($2))
 $$$$(eval saved_var_$1_changed=1)
 $$$$(eval saved_var_$1_value:=$$$$($2))
 $$$$(call mksucc, Updated variable cache for $1)
-)
+$(empty)endif
 $(empty)endef
 $(call defer,saved-var-$1-restore)
 endef
