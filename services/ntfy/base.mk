6843d94fe72949fe60c3bc2b94fba198ef4a917c
xtex <xtexchooser@duck.com>
2024-02-17 21:40:56 +0800

    ntfy: Fix directories

--- services/ntfy/base.mk
+++ services/ntfy/base.mk
@@ -3,6 +3,7 @@ $(call load-state, services/caddy)
 $(call x-container-service)
 V_SERVICE	= ntfy
 V_DEPS		+= /etc/ntfy/server.yml
+V_DEPS		+= /var/run/ntfy /var/lib/ntfy /var/cache/ntfy /var/log/ntfy
 V_ARGS		+= --mount=type=bind,src=/etc/ntfy,dst=/etc/ntfy,ro=true
 V_ARGS		+= --mount=type=bind,src=/var/run/ntfy,dst=/var/run/ntfy
 V_ARGS		+= --mount=type=bind,src=/var/lib/ntfy,dst=/var/lib/ntfy
@@ -18,3 +19,8 @@ V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/ntfy/server.yml
 $(call end)
 
 CADDY_INCLUDES += $(STATES_DIR)/services/ntfy/Caddyfile
+
+$(call add-fs-directory,/var/run/ntfy)
+$(call add-fs-directory,/var/lib/ntfy)
+$(call add-fs-directory,/var/cache/ntfy)
+$(call add-fs-directory,/var/log/ntfy)
