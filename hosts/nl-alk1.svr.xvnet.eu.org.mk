3d4ae1822b4180facd7c50fe08645689530135d8
xtex <xtexchooser@duck.com>
2024-02-17 23:30:44 +0800

    Add routing properties

--- hosts/nl-alk1.svr.xvnet.eu.org.mk
+++ hosts/nl-alk1.svr.xvnet.eu.org.mk
@@ -1,3 +1,8 @@
+BIRD_ROUTER_ID := 5.255.109.94
+DN42_LOCAL_IP := 172.20.206.65
+XVNET_ASN := 4244310001
+XVNET_LOCAL_IP := fd00:443a:ef14:1::1
+
 $(call load-state, services/bird)
 $(call load-state, services/caddy)
 $(call load-state, services/ntfy)
