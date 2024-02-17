7fe355b8c6f58f98fff219f10df809c082f5bb56
xtex <xtexchooser@duck.com>
2024-02-17 17:38:51 +0800

    caddy: Take control of caddyfile

--- services/caddy/base.mk
+++ services/caddy/base.mk
@@ -1,5 +1,6 @@
 $(call x-container-service)
 V_SERVICE	= caddy
+V_DEPS		+= /etc/caddy/Caddyfile
 V_ARGS		+= --cap-add=CAP_NET_BIND_SERVICE
 V_ARGS		+= --env HOME=/root
 V_ARGS		+= --hostname=caddy
@@ -9,4 +10,11 @@ V_ARGS		+= --mount=type=bind,src=/var/lib/caddy,dst=/root/.local/share/caddy
 V_ARGS		+= --publish=80:80
 V_ARGS		+= --publish=443:443
 V_ARGS 		+= codeberg.org/xvnet/x-caddy
+$(call end)
+
+CADDY_INCLUDES ?=
+$(call fs-file)
+V_PATH		= /etc/caddy/Caddyfile
+V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/caddy/Caddyfile
+V_DEP_VARS	+= CADDY_INCLUDES
 $(call end)
