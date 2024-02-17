cbbc4b6a0094a33ab23c46da8a8d93ad66c91bd3
xtex <xtexchooser@duck.com>
2024-02-17 19:39:36 +0800

    saved-var: Fix saved-var cache always invalidated

--- make/02-saved-var.mk
+++ make/02-saved-var.mk
@@ -10,16 +10,16 @@ $(saved-var-$1):
 	@$(MKDIR) -p $(VARS_DIR)
 	echo "saved_var_$1_value=$$($2)" > $$@
 
-$ define saved-var-$1-restore
+$(empty)define saved-var-$1-restore
 $$$$(eval -include $(saved-var-$1))
-$ ifneq ($$$$(saved_var_$1_value),$$($2))
-$$$$(file >$$$$(saved-var-$1),saved_var_$1_value=$$($2))
+$(empty)$$$$(if $$$$(call strneq,$$$$(saved_var_$1_value),$$$$($2)),
+$$$$(file > $$$$(saved-var-$1),saved_var_$1_value=$$$$($2))
 $$$$(call mktrace, Invalidated variable cache $1. Old: $$$$(saved_var_$1_value) New: $$($2))
 $$$$(eval saved_var_$1_changed=1)
-$$$$(eval saved_var_$1_value:=$$($2))
+$$$$(eval saved_var_$1_value:=$$$$($2))
 $$$$(call mksucc, Updated variable cache for $1)
-$ endif
-$ endef
+)
+$(empty)endef
 $(call defer,saved-var-$1-restore)
 endef
 
