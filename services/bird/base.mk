42697e2d30c9a7a96cc038e942f943a0ae2857cf
xtex <xtexchooser@duck.com>
2024-02-18 09:14:04 +0800

    bird: Add all deps to bird conf

--- services/bird/base.mk
+++ services/bird/base.mk
@@ -24,4 +24,5 @@ V_PATH		= /etc/bird/bird.conf
 V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/bird/conf/bird.conf
 V_DEP_VARS	+= BIRD_INCLUDES
 V_DEP_VARS	+= BIRD_ROUTER_ID DN42_LOCAL_IP XVNET_ASN XVNET_LOCAL_IP
+V_DEPS		+= $(wildcard $(STATES_DIR)/services/bird/conf/*.conf)
 $(call end)
