482f6d81132ccf9fae1ea5adb2c7a12e56f55295
xtex <xtexchooser@duck.com>
2024-02-18 10:23:55 +0800

    bird: Keep DN42 ROA updater log

--- services/bird/script/update-dn42-roa.sh
+++ services/bird/script/update-dn42-roa.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
+exec &>>/var/log/dn42-roa-updater.log
 curl -SL -o /etc/bird/roa_dn42.conf 'https://explorer.burble.com/api/roa/bird/2/4'
 curl -SL -o /etc/bird/roa_dn42_v6.conf 'https://explorer.burble.com/api/roa/bird/2/6'
