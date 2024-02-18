b319d46603aaedf1e97545fca45f2d0301c0ac4c
xtex <xtexchooser@duck.com>
2024-02-18 10:24:05 +0800

    bird: Reconf BIRD after DN42 ROA update

--- services/bird/script/update-dn42-roa.sh
+++ services/bird/script/update-dn42-roa.sh
@@ -3,3 +3,4 @@ set -e
 exec &>>/var/log/dn42-roa-updater.log
 curl -SL -o /etc/bird/roa_dn42.conf 'https://explorer.burble.com/api/roa/bird/2/4'
 curl -SL -o /etc/bird/roa_dn42_v6.conf 'https://explorer.burble.com/api/roa/bird/2/6'
+"$(dirname "$0")/reconf.sh"
