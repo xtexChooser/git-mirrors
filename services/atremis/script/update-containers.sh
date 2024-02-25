c71997867d88d9765ec09bd309d7e7f59dd4e615
xtex <xtexchooser@duck.com>
2024-02-25 13:32:45 +0800

    atremis: Add container images updater

---
+++ services/atremis/script/update-containers.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+updated=""
+for img in $(podman image ls --format "{{if .RepoTags}}{{index .RepoTags 0}}{{end}}"); do
+	oldDigest="$(podman ls "$img" --format "{{.Digest}}")"
+	podman pull "$img"
+	newDigest="$(podman ls "$img" --format "{{.Digest}}")"
+
+	if [[ "$oldDigest" != "$newDigest" ]]; then
+		echo Updated container image "$img"
+		updated=true
+		for svc in $(podman container ls --format "{{if eq .Image \"$img\"}}{{index .Labels \"org.eu.xvnet.x.dinitservice\"}}{{end}}"); do
+			[[ "$svc" == "" ]] && continue
+			echo Restarting container service "$svc"
+			dinitctl restart "$svc"
+		done
+	fi
+done
+
+if [[ -n "$updated" ]]; then
+	atre pull
+	podman image prune -f
+	atre apply
+fi
