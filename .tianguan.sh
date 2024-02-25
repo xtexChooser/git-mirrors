9af08cb83186fe4b607918991d2a148b9aae0b48
xtex <xtexchooser@duck.com>
2024-02-25 11:23:35 +0800

    atremis: Setup secrets on servers

--- .tianguan.sh
+++ .tianguan.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
 tiang::target nl-alk1 ssh://nl-alk1.svr.xvnet.eu.org
+
+tiangCommandsUsage+="""
+    -ss --syncsec  [FILE]       Copy a secret file to targets
+"""
+tiang::defineCommand atremis::syncsec
+
+atremis::syncsec() {
+	if [[ "$1" == "-ss" || "$1" == "--syncsec" ]]; then
+		[ $# -lt 2 ] && tiang::error "1 parameter is required for --syncsec"
+		tiang::runParallelOnTargets tiang::runSCP "../secrets/$2" "/srv/secrets/$2"
+		tiangHandledParams=2
+	else
+		tiangHandledParams=0
+	fi
+	exit 0
+}
