656e85286d53a83eac2331b0bf70a6ef4b4c9ed5
xtex <xtexchooser@duck.com>
2024-02-25 13:27:15 +0800

    atremis: Write dinit service name to container label

--- modules/containers.mk
+++ modules/containers.mk
@@ -1,8 +1,9 @@
-X_CONTAINER_SERVICE_VARS = V_TARGET_NAME V_SERVICE V_STOPPED V_ARGS V_POST V_SVCDEPS $(v-deps-var)
+X_CONTAINER_SERVICE_VARS = V_TARGET_NAME V_SERVICE V_STOPPED V_ARGS V_POST V_SVCDEPS V_AUTO_UPDATE V_PRE_START V_POST_START V_PRE_STOP V_POST_STOP $(v-deps-var)
 
 define x-container-service0
 $(eval V_PIDFILE?=/var/run/containers/$(V_SERVICE).pid)
 $(eval V_DEP_VARS+=$(addprefix x-container-$(V_SERVICE)-,args start-cmd stop-cmd))
+$(eval V_AUTO_UPDATE?=true)
 $(eval V_PRE_START?=true)
 $(eval V_POST_START?=true)
 $(eval V_PRE_STOP?=true)
@@ -10,6 +11,7 @@ $(eval V_POST_STOP?=true)
 $(eval x-container-$(V_SERVICE)-args:=$(V_ARGS))
 $(eval x-container-$(V_SERVICE)-start-cmd:=$(V_PRE_START); $(PODMAN) container run \
 	--name $(V_SERVICE) --rm -d --pidfile=$(V_PIDFILE) --replace \
+	--label=org.eu.xvnet.x.dinitservice=$(V_SERVICE) \
 	--hostname=$(V_SERVICE) \
 	$(V_ARGS); $(V_POST_START))
 $(eval x-container-$(V_SERVICE)-stop-cmd:=$(V_PRE_STOP); $(PODMAN) container rm -f -i $(V_SERVICE); \
@@ -39,6 +41,8 @@ $$(call fs-file)
 V_PATH		= $$(DINITD_DIR)/boot.d/$(V_SERVICE)
 $(if $(call not,$(call is-true,$(V_STOPPED))),V_SYMLINK = ../$(V_SERVICE),V_EXIST = n)
 $$(call end)
+
+$(if $(call is-true,$(V_AUTO_UPDATE)),CONTANIERS_AUTO_UPDATE+=$1)
 
 $(call unset-vars)
 endef
