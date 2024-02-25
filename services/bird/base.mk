d1fa7aedce7dfb05417a093805e3fdac5fb10f64
xtex <xtexchooser@duck.com>
2024-02-25 14:17:28 +0800

    BIRD: Remove unused BIRD_INCLUDES

--- services/bird/base.mk
+++ services/bird/base.mk
@@ -25,11 +25,9 @@ V_CMD		= $(STATES_DIR)/services/bird/script/reconf.sh
 V_DEPS		+= /etc/bird/bird.conf
 $(call end)
 
-BIRD_INCLUDES :=
 $(call fs-file)
 V_PATH		= /etc/bird/bird.conf
 V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/bird/conf/bird.conf
-V_DEP_VARS	+= BIRD_INCLUDES
 V_DEP_VARS	+= BIRD_ROUTER_ID XVNET_NUM DN42_LOCAL_IP XVNET_ASN XVNET_LOCAL_IP
 V_DEPS		+= $(wildcard $(STATES_DIR)/services/bird/conf/*.conf)
 $(call end)
