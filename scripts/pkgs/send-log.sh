bbdb104f4e18e142ad763fd7cbbaa54aa00aa035
xtex <xtexchooser@duck.com>
2024-03-02 17:29:40 +0800

    Merge pkgs

---
+++ scripts/pkgs/send-log.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+set -e
+
+command -q -v curl || apk add curl
+
+[[ "${CI_WORKFLOW_NAME:-}" == "" ]] && echo "CI_WORKFLOW_NAME is not available" && exit 1
+[[ "${CI_PIPELINE_STATUS:-}" == "" ]] && echo "CI_PIPELINE_STATUS is not available" && exit 1
+[[ "${CI_PIPELINE_NUMBER:-}" == "" ]] && echo "CI_PIPELINE_NUMBER is not available" && exit 1
+[[ "${CI_PIPELINE_EVENT:-}" == "" ]] && echo "CI_PIPELINE_EVENT is not available" && exit 1
+[[ "${CI_STEP_URL:-}" == "" ]] && echo "CI_STEP_URL is not available" && exit 1
+[[ "${NTFY_TOKEN:-}" == "" ]] && echo "NTFY_TOKEN is not available" && exit 1
+
+pkg=$CI_WORKFLOW_NAME
+if [[ "$VERSION" != "" ]]; then
+	pkgWithVersion="$pkg $VERSION"
+else
+	pkgWithVersion="$pkg"
+fi
+
+desc="Pipeline: $CI_PIPELINE_NUMBER
+Triggerred by: $CI_PIPELINE_EVENT"
+priority=min
+
+if [[ "$CI_PIPELINE_STATUS" == "success" ]]; then
+	title="pkgs: $pkgWithVersion build succeeded"
+elif [[ "$CI_PIPELINE_STATUS" == "failure" ]]; then
+	title="pkgs: $pkgWithVersion build failed"
+	priority=high
+else
+	title="pkgs: Pipeline status unknown ($CI_PIPELINE_STATUS): $pkgWithVersion"
+	priority=high
+fi
+
+curl \
+	-H "Authorization: Bearer $NTFY_TOKEN" \
+	-H "Title: $title" \
+	-H "Actions: view, View on CI, $CI_STEP_URL" \
+	-H "Tags: pkgbld,service-$pkg,pkgbld-$CI_PIPELINE_STATUS" \
+	-H "Priority: $priority" \
+	-d "$desc" \
+	-SL \
+	https://ntfy.xvnet.eu.org/publogs
---
+++ scripts/pkgs/tag-latest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+repo="$1"
+version="$(grep -F '# TAG:LATEST' <"$2" | cut -d'-' -f2 | cut -d'#' -f1 | xargs)"
+pkgversion="$3"
+if [ "$version" == "$pkgversion" ]; then
+	command -q -v skopeo || apk add skopeo
+	echo Logging into registry
+	skopeo login -u "$CODEBERG_TOKEN" -p "$CODEBERG_TOKEN" codeberg.org
+	echo Copying to latest
+	skopeo copy -a "docker://$repo:$pkgversion" "docker://$repo:latest"
+fi
