611c280bb0312bc29f34dca27ae76b334cdd2240
xtex <xtexchooser@duck.com>
2024-03-03 10:03:37 +0800

    BIND: Mark dns-root-zone image as dependency

--- services/bind/base.mk
+++ services/bind/base.mk
@@ -5,6 +5,7 @@ V_SVCDEPS	+= /var/run/bind podman-image-dns-root-zone
 V_ARGS		+= --mount=type=bind,src=/etc/bind,dst=/etc/bind,ro=true
 V_ARGS		+= --mount=type=bind,src=/var/run/bind,dst=/var/run/bind
 V_ARGS		+= --mount=type=image,source=codeberg.org/xvnet/dns-root-zone:latest,destination=/opt/root-zone
+V_ARGS		+= --label=org.eu.xvnet.x.depimgs=codeberg.org/xvnet/dns-root-zone:latest
 V_ARGS		+= --publish-all
 V_ARGS		+= --network=host --cap-add=CAP_NET_BIND_SERVICE
 V_ARGS 		+= codeberg.org/xvnet/bind:latest
