a1cb2ddd069f7efb2a3208aa1ffd959b436771ef
xtex <xtexchooser@duck.com>
2024-03-03 11:35:08 +0800

    fs-line: Fix prepending

--- modules/fs-line.mk
+++ modules/fs-line.mk
@@ -1,8 +1,8 @@
 FS_LINE_VARS = V_TARGET_NAME V_NAME V_POST $(v-deps-var) V_PATH V_FLAGS V_MATCH V_LINE V_PREPEND
 define fs-line0
 $(eval V_TARGET_NAME?=fs-line-$(V_PATH)-$(V_NAME))
-$(eval V_LINE?=$(subst $$,$$$$,$(subst ','"'"',$(V_LINE))))
-$(eval V_MATCH?=$(subst $$,$$$$,$(subst ','"'"',$(V_MATCH))))
+$(eval V_LINE:=$(subst $$,$$$$$$$$$$$$$$$$,$(subst ','"'"',$(V_LINE))))
+$(eval V_MATCH:=$(subst $$,$$$$$$$$$$$$$$$$,$(subst ','"'"',$(V_MATCH))))
 
 $(call mktrace, Define exist fs-line target: $(V_UNIT))
 $(call mktrace-vars,$(FS_LINE_VARS))
@@ -15,16 +15,16 @@ $(call vt-target,$(V_TARGET_NAME))
 $(V_TARGET_NAME): $(if $(call not,$(findstring no-dep,$(V_FLAGS))$(findstring append,$(V_FLAGS))),$(V_PATH)) $(v-deps)
 	export E_MAJOR=fs-line E_NAME=$(V_NAME) E_PATH=$(V_PATH)
 	if [[ -e $(V_PATH) ]]; then
-		if ! grep -F 'V_LINE' $(V_PATH) $(DROP_STDOUT); then
-			if $(if $(V_MATCH),grep -E 'V_MATCH' $(V_PATH) $(DROP_STDOUT), false); then
+		if ! grep -F '$(V_LINE)' $(V_PATH) $(DROP_STDOUT); then
+			if $(if $(V_MATCH),grep -E '$(V_MATCH)' $(V_PATH) $(DROP_STDOUT), false); then
 				$(MV) $(V_PATH) $(V_PATH).bak
-				$(SED) -E -e 's/V_MATCH/V_LINE/g' $(V_PATH).bak > $(V_PATH)
+				$(SED) -E -e 's/$(V_MATCH)/$(V_LINE)/g' $(V_PATH).bak > $(V_PATH)
 				$(call succ, Replaced line with '$(V_LINE)' in $(V_PATH))
 				$(call vpost, E_MINOR=replaced)
 			else
 				$(CP) $(V_PATH) $(V_PATH).bak
-				$(if $(call is-true,$(V_PREPEND)),{ echo 'V_LINE'; cat $(V_PATH); } > $(V_PATH).newtmp; mv $(V_PATH).newtmp $(V_PATH),echo 'V_LINE' >> $(V_PATH))
-				$(call succ, Appended line '$(V_LINE)' to $(V_PATH))
+				$(if $(call is-true,$(V_PREPEND)),{ echo '$(V_LINE)'; cat $(V_PATH); } > $(V_PATH).newtmp; mv $(V_PATH).newtmp $(V_PATH),echo 'V_LINE' >> $(V_PATH))
+				$(call succ, $(if $(call is-true,$(V_PREPEND)),Prepended,Appended) line '$(V_LINE)' to $(V_PATH))
 				$(call vpost, E_MINOR=appended)
 			fi
 		fi
