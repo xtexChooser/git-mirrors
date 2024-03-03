618b51ffa8a44b7ed27d3ee93170d23a93025802
xtex <xtexchooser@duck.com>
2024-03-03 09:29:22 +0800

    BIND: Mount root zone to /opt/root-zone

--- services/bind/base.mk
+++ services/bind/base.mk
@@ -4,6 +4,7 @@ V_DEPS		+= /etc/bind/named.conf
 V_SVCDEPS	+= /var/run/bind
 V_ARGS		+= --mount=type=bind,src=/etc/bind,dst=/etc/bind,ro=true
 V_ARGS		+= --mount=type=bind,src=/var/run/bind,dst=/var/run/bind
+V_ARGS		+= --mount=type=image,source=codeberg.org/xvnet/dns-root-zone:latest,destination=/opt/root-zone
 V_ARGS		+= --publish-all
 V_ARGS		+= --network=host --cap-add=CAP_NET_BIND_SERVICE
 V_ARGS 		+= codeberg.org/xvnet/bind:latest
