3ebbfdc2666a29e412ecce3e80e65e11716852d1
xtex <xtexchooser@duck.com>
2024-03-03 09:49:55 +0800

    BIND: Pull dns-root-zone image

--- services/bind/base.mk
+++ services/bind/base.mk
@@ -1,7 +1,7 @@
 $(call x-container-service)
 V_SERVICE	= bind
 V_DEPS		+= /etc/bind/named.conf
-V_SVCDEPS	+= /var/run/bind
+V_SVCDEPS	+= /var/run/bind podman-image-dns-root-zone
 V_ARGS		+= --mount=type=bind,src=/etc/bind,dst=/etc/bind,ro=true
 V_ARGS		+= --mount=type=bind,src=/var/run/bind,dst=/var/run/bind
 V_ARGS		+= --mount=type=image,source=codeberg.org/xvnet/dns-root-zone:latest,destination=/opt/root-zone
@@ -16,4 +16,9 @@ $(call fs-file)
 V_PATH		= /etc/bind/named.conf
 V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/bind/conf/named.conf
 V_DEPS		+= $(wildcard $(STATES_DIR)/services/bind/conf/*.conf)
+$(call end)
+
+$(call podman-image)
+V_NAME		= dns-root-zone
+V_IMAGE		= codeberg.org/xvnet/dns-root-zone:latest
 $(call end)
