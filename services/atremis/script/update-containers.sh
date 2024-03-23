0b54d1c31b70b2c6a6d28362a6dba868a4440632
xtex <xtexchooser@duck.com>
2024-03-23 19:34:25 +0800

    atremis: Restart all services at once

--- services/atremis/script/update-containers.sh
+++ services/atremis/script/update-containers.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+declare -a servicesToRestart
+
 updated=""
 for img in $(podman image ls --format "{{if .RepoTags}}{{index .RepoTags 0}}{{end}}"); do
 	oldDigest="$(podman image ls "$img" --format "{{.Digest}}")"
@@ -12,13 +14,17 @@ for img in $(podman image ls --format "{{if .RepoTags}}{{index .RepoTags 0}}{{en
 		updated=true
 		for svc in $(podman container ls --format "{{json .}}" | jq -s -r ".[] | select((.Labels[\"org.eu.xvnet.x.depimgs\"]? // \"\" | split(\",\") | contains([\"$img\"])) or (.Image == \"$img\")) | .Labels[\"org.eu.xvnet.x.dinitservice\"]? // \"\""); do
 			[[ "$svc" == "" ]] && continue
-			echo Restarting container service "$svc"
-			dinitctl restart "$svc"
+			servicesToRestart=("${servicesToRestart[@]}" "$svc")
 		done
 	fi
 done
 
 if [[ -n "$updated" ]]; then
+	while IFS= read -r -d '' svc; do
+		echo Restarting container service "$svc"
+		dinitctl restart "$svc"
+	done < <(printf "%s\0" "${servicesToRestart[@]}" | sort -uz)
+
 	atre pull
 	podman image prune -f
 	atre apply
