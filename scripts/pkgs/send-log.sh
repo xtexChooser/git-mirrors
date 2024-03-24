4feea46ba71e441aac384d7b30a6cfb3ec4e46b6
xtex <xtexchooser@duck.com>
2024-03-24 14:24:48 +0800

    DNS: Preview DNSControl changes

--- scripts/pkgs/send-log.sh
+++ scripts/pkgs/send-log.sh
@@ -10,11 +10,9 @@ command -v curl >/dev/null 2>&1 || apk add curl
 [[ "${CI_STEP_URL:-}" == "" ]] && echo "CI_STEP_URL is not available" && exit 1
 [[ "${NTFY_TOKEN:-}" == "" ]] && echo "NTFY_TOKEN is not available" && exit 1
 
-pkg=${CI_WORKFLOW_NAME#build-}
+pipeline=${CI_WORKFLOW_NAME#build-}
 if [[ "$VERSION" != "" ]]; then
-	pkgWithVersion="$pkg $VERSION"
-else
-	pkgWithVersion="$pkg"
+	pipeline="$pipeline $VERSION"
 fi
 
 desc="Pipeline: $CI_PIPELINE_NUMBER
@@ -22,21 +20,21 @@ Triggerred by: $CI_PIPELINE_EVENT"
 priority=min
 
 if [[ "$CI_PIPELINE_STATUS" == "success" ]]; then
-	title="pkgs: $pkgWithVersion build succeeded"
+	title="$pipeline build succeeded"
 elif [[ "$CI_PIPELINE_STATUS" == "failure" ]]; then
-	title="pkgs: $pkgWithVersion build failed"
+	title="$pipeline build failed"
 	priority=high
 else
-	title="pkgs: Pipeline status unknown ($CI_PIPELINE_STATUS): $pkgWithVersion"
+	title="pipelines: Pipeline status unknown ($CI_PIPELINE_STATUS): $pipeline"
 	priority=high
 fi
 
 curl \
 	-H "Authorization: Bearer $NTFY_TOKEN" \
-	-H "Title: $title" \
-	-H "Actions: view, View on CI, $CI_STEP_URL" \
-	-H "Tags: pkgbld,service-$pkg,pkgbld-$CI_PIPELINE_STATUS" \
-	-H "Priority: $priority" \
+	-H "X-Title: $title" \
+	-H "X-Actions: view, View on CI, $CI_STEP_URL" \
+	-H "X-Tags: pipline-$pipeline,pipeline-$CI_PIPELINE_STATUS" \
+	-H "X-Priority: $priority" \
 	-d "$desc" \
 	-SL \
 	https://ntfy.xvnet.eu.org/publogs
