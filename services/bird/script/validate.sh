d061f4fa5bd8cca1b7511003a4c8cb0e197af3f7
xtex <xtexchooser@duck.com>
2024-03-24 13:43:10 +0800

    BIRD: Validate configuration with containered BIRD

--- services/bird/script/validate.sh
+++ services/bird/script/validate.sh
@@ -5,5 +5,10 @@ set -e
 make HOSTNAME=opilio.s.xvnet0.eu.org USER=root LEONIS_LOAD_ALL=y do-tpl TPL_BACKEND=bash-tpl \
 	TPL_IN=services/bird/conf/bird.conf TPL_OUT=.bird-valid.conf
 sed -i -e 's/include/#include/' .bird-valid.conf
-bird -p -c .bird-valid.conf
+
+podman run -it --rm --name bird-validate -v "$(pwd)":/validate \
+	--privileged \
+	codeberg.org/xvnet/bird:latest \
+	-p -c /validate/.bird-valid.conf
+
 rm -f .bird-valid.conf
