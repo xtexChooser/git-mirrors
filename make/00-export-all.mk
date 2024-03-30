5eb726b96ca8cacbc758d187d4dd0dced5f4c4e6
xtex <xtexchooser@duck.com>
2024-03-30 21:03:22 +0800

    Revert "Keep variables starts with A_ unexported"
    This reverts commit 297e41433fea1a4a618b3b8741614d83a4708385.

--- make/00-export-all.mk
+++ make/00-export-all.mk
@@ -1,7 +1,7 @@
 # .EXPORT_ALL_VARIABLES leaks too many macros
 
 define should-export
-$(and $(call not,$(call starts-with,A_,$1)),$(call not,$(subst A,,$(subst B,,$(subst C,,$(subst D,,$(subst E,,$(subst F,,$(subst G,,$(subst H,,$(subst I,,$(subst J,,$(subst K,,$(subst L,,$(subst M,,$(subst N,,$(subst O,,$(subst P,,$(subst Q,,$(subst R,,$(subst S,,$(subst T,,$(subst U,,$(subst V,,$(subst W,,$(subst X,,$(subst Y,,$(subst Z,,$(subst 0,,$(subst 1,,$(subst 2,,$(subst 3,,$(subst 4,,$(subst 5,,$(subst 6,,$(subst 7,,$(subst 8,,$(subst 9,,$(subst _,,$1)))))))))))))))))))))))))))))))))))))))
+$(call not,$(subst A,,$(subst B,,$(subst C,,$(subst D,,$(subst E,,$(subst F,,$(subst G,,$(subst H,,$(subst I,,$(subst J,,$(subst K,,$(subst L,,$(subst M,,$(subst N,,$(subst O,,$(subst P,,$(subst Q,,$(subst R,,$(subst S,,$(subst T,,$(subst U,,$(subst V,,$(subst W,,$(subst X,,$(subst Y,,$(subst Z,,$(subst 0,,$(subst 1,,$(subst 2,,$(subst 3,,$(subst 4,,$(subst 5,,$(subst 6,,$(subst 7,,$(subst 8,,$(subst 9,,$(subst _,,$1))))))))))))))))))))))))))))))))))))))
 endef
 
 define export-all
