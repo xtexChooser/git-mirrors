9ac9d38b4008ecf864c99afa7d6c54038711953c
xtex <xtexchooser@duck.com>
2024-03-30 21:41:15 +0800

    DNS(2a0e:b107:16c0): Push DNS records to GCore

--- services/dns/dnscontrol/dnsconfig.js
+++ services/dns/dnscontrol/dnsconfig.js
@@ -10,6 +10,9 @@ D_EXTEND("xvnet0.eu.org", DnsProvider(DSP_DESEC, 0), DnsProvider(DSP_GCORE, 0));
 require("converted/xvnet.eu.org.js");
 D_EXTEND("xvnet.eu.org", DnsProvider(DSP_GCORE, 0));
 
+require("converted/0.c.6.1.7.0.1.b.e.0.a.2.ip6.arpa.js");
+D_EXTEND("0.c.6.1.7.0.1.b.e.0.a.2.ip6.arpa", DnsProvider(DSP_GCORE, 0));
+
 // var domains = getConfiguredDomains();
 // for (i = 0; i < domains.length; i++)
 // 	D_EXTEND(domains[i], DnsProvider(DSP_BIND));
