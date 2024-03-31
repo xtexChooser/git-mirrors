4a03319130e2181d8d24b856b57667bff13101a1
xtex <xtexchooser@duck.com>
2024-03-31 14:56:04 +0800

    PG(balaro): Fix DB init check

--- services/postgres/balaro/script/initdb.sh
+++ services/postgres/balaro/script/initdb.sh
@@ -10,5 +10,6 @@ podman run -it --rm --user "root:root" \
 	-v /var/lib/postgresql/balaro/data:/var/lib/postgresql/data \
 	-e PGDATA=/var/lib/postgresql/data \
 	-e POSTGRES_PASSWORD="$password" \
-	codeberg.org/xvnet/postgres
+	codeberg.org/xvnet/postgres || true
 rm -rf /var/lib/postgresql/balaro/data/{postgres.conf,postgresql.conf,pg_ident.conf,pg_hba.conf}
+cp /var/lib/postgresql/balaro/data/PG_VERSION /var/lib/postgresql/balaro/data/
