1debfc0050d5d1a3fc090f36c5cab159b04e0439
xtex <xtexchooser@duck.com>
2024-04-03 20:56:06 +0800

    Add pre and post hooks

--- make/02-targets.mk
+++ make/02-targets.mk
@@ -29,19 +29,24 @@ $(filter $1,$(leonis-requested-states))
 endef
 
 define load-requested-states
+$(foreach state,$(leonis-requested-states),$(eval -include $(STATES_DIR)/$(state)/pre.mk))
 $(foreach state,$(leonis-requested-states),$(call load-state-now,$(state)))
+$(foreach state,$(leonis-requested-states),$(eval -include $(STATES_DIR)/$(state)/post.mk))
 endef
 
 define load-state-now0
-$(if $(call is-state-loaded,$1),,
 $(call mktrace, Loading vendor state $1)
 leonis-loaded-states += $1
 $(empty)-include $(STATES_DIR)/$1.mk
 $(empty)-include $(STATES_DIR)/$1/base.mk
-)
 endef
 $(call define-inline-func,load-state-now)
 
 define is-state-loaded
 $(filter $1,$(leonis-loaded-states))
+endef
+
+define invoke-hooks
+$(call mktrace, Calling state hook $1)
+$(foreach state,$(leonis-requested-states),-include $(STATES_DIR)/$(state)/hooks/$1.mk)
 endef
