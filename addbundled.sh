26d9b4063464d1c6414086aca1816055ddd1f598
xtex <xtexchooser@duck.com>
2024-04-04 23:01:44 +0800

    Add addbundled.sh

---
+++ addbundled.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+grep -F '[submodule ' mw/.gitmodules | grep -Eo '(extensions|skins)/[^"]*' | while read -r n; do
+	n="${n/\// }"
+	kind="$(cut -d' ' -f1 <<<"$n")"
+	name="$(cut -d' ' -f2 <<<"$n")"
+	./addext.sh "$kind" "$name" 2>>.addbundled.log || true
+	git push
+done
+
+echo "Added, errors:"
+
+if [[ -e ".addbundled.log" ]]; then
+	cat .addbundled.log >&2
+	rm .addbundled.log
+fi
