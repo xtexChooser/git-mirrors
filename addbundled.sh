c887f2e119b2e62af705ab5eef623769312a581d
xtex <xtexchooser@duck.com>
2024-04-04 23:09:11 +0800

    addbundled.sh: Improve performance

--- addbundled.sh
+++ addbundled.sh
@@ -3,10 +3,11 @@
 set -e
 
 grep -F '[submodule ' mw/.gitmodules | grep -Eo '(extensions|skins)/[^"]*' | while read -r n; do
+	[[ ! -e "mw/$n" ]] || continue
 	n="${n/\// }"
 	kind="$(cut -d' ' -f1 <<<"$n")"
 	name="$(cut -d' ' -f2 <<<"$n")"
-	./addext.sh "$kind" "$name" 2>>.addbundled.log || true
+	./addext.sh "$kind" "$name" 2>>.addbundled.log || continue
 	git push
 done
 
