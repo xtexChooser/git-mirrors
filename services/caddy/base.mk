de66d9ea72fe70c3b0a4cc65af5a3ae3e2436e7b
xtex <xtexchooser@duck.com>
2024-04-05 10:32:52 +0800

    MW, Caddy: Mount MW image to Caddy container

--- services/caddy/base.mk
+++ services/caddy/base.mk
@@ -9,8 +9,8 @@ V_ARGS		+= --mount=type=bind,src=/var/lib/caddy,dst=/root/.local/share/caddy
 V_ARGS		+= --publish=80:80/tcp --publish=80:80/udp
 V_ARGS		+= --publish=443:443/tcp --publish=443:443/udp
 V_ARGS		+= --memory=64M
+$(call invoke-hooks,caddy-container-opts)
 V_ARGS 		+= codeberg.org/xvnet/x-caddy
-$(call invoke-hooks,caddy-container)
 $(call end)
 
 CADDY_INCLUDES :=
---
+++ services/mediawiki/hooks/caddy-container-opts.mk
@@ -0,0 +1,2 @@
+V_ARGS		+= --mount=type=image,source=codeberg.org/xvnet/x-mediawiki:latest,destination=/opt/mediawiki
+V_ARGS		+= --label=org.eu.xvnet.x.depimgs=codeberg.org/xvnet/x-mediawiki:latest
