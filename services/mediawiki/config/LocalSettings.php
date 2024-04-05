29819301018a5582ec38eff3d5656659991a6228
xtex <xtexchooser@duck.com>
2024-04-05 10:43:04 +0800

    MW: Add MW config

--- services/mediawiki/config/LocalSettings.php
+++ services/mediawiki/config/LocalSettings.php
@@ -1,4 +1,22 @@
 <?php
-if ( !defined( 'MEDIAWIKI' ) ) {
+if (!defined('MEDIAWIKI')) {
 	exit;
 }
+
+$wikis = [
+	'wikimeta',
+	'wikixvnet',
+];
+
+if (defined('MW_DB')) {
+	$wikiID = MW_DB;
+} else {
+	$wikiID = $_SERVER['MW_DB'] ?? null;
+	if (!$wikiID) {
+		die('Unknown wiki.');
+	}
+}
+
+$wgLocalDatabases = $wgConf->wikis = $wikis;
+$wgDBname = $wikiID;
+$wgDBuser = 'mediawiki';
