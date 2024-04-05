63e79a30ccbfdeab8a932efaa9af358358f8230e
xtex <xtexchooser@duck.com>
2024-04-05 16:52:02 +0800

    MW: Fix grant access

--- services/mediawiki/script/createwiki.sh
+++ services/mediawiki/script/createwiki.sh
@@ -18,8 +18,8 @@ runSQL() {
 echo "Creating wiki $1"
 
 runSQL CREATE DATABASE wiki"$1"
-runSQL GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mediawiki
-runSQL GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mediawiki
+runMW php maintenance/sql.php --wiki "$1" --query "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mediawiki"
+runMW php maintenance/sql.php --wiki "$1" --query "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mediawiki"
 runMW php maintenance/sql.php --wiki "$1" maintenance/postgres/tables-generated.sql
 runMW php maintenance/run.php --wiki "$1" update --quick
 
