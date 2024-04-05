c80274fb5d8ae5441aafc3fab412d6f7c3911559
xtex <xtexchooser@duck.com>
2024-04-05 12:14:10 +0800

    MW: Set $wgDBserver

--- services/mediawiki/config/Database.php
+++ services/mediawiki/config/Database.php
@@ -6,5 +6,7 @@ $wgLocalDatabases = $wgConf->wikis = array_map(function ($n) {
 
 $wgDBtype = 'postgres';
 $wgDBname = 'wiki' . $wikiID;
+$wgDBserver = 'opilio.s.xvnet0.eu.org';
+$wgDBport = 5433;
 $wgDBuser = 'mediawiki';
 $wgDBadminuser = 'mediawikiadmin';
