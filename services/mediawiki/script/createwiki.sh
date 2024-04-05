867ca5e1a4b3f6c17a494146b38deab49502edcb
xtex <xtexchooser@duck.com>
2024-04-05 15:30:15 +0800

    MW: Fix maintement script

--- services/mediawiki/script/createwiki.sh
+++ services/mediawiki/script/createwiki.sh
@@ -6,15 +6,17 @@ runMW() {
 	podman exec -it mediawiki "$@"
 }
 
-:"${RUN_AS_WIKI:=meta}"
+: "${RUN_AS_WIKI:=meta}"
 runSQL() {
-	runMW php maintenance/sql.php --wiki "$RUN_AS_WIKI" --query "$*"
+	runMW php maintenance/sql.php --wiki "$RUN_AS_WIKI" --query "$*;"
 }
+
+echo "Creating wiki $1"
 
 runSQL CREATE DATABASE wiki"$1"
 runSQL GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO mediawiki
 runSQL GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO mediawiki
-runMW php maintenance/sql.php --wiki "$RUN_AS_WIKI" maintenance/postgres/tables-generated.sql
-runMW php maintenance/run.php --wiki "$RUN_AS_WIKI" update --quick
+runMW php maintenance/sql.php --wiki "$1" maintenance/postgres/tables-generated.sql
+runMW php maintenance/run.php --wiki "$1" update --quick
 
 echo "Wiki $1 initialized"
