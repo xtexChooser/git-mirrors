7ec613f655cb19721e17ec0fe43358d61dbd8ae1
xtex <xtexchooser@duck.com>
2024-04-20 20:36:04 +0800

    monto: Move sock files to /var/run/mariadb/monto

--- services/mariadb/monto/base.mk
+++ services/mariadb/monto/base.mk
@@ -3,7 +3,7 @@ V_SERVICE	= monto
 V_DEPS		+= /var/lib/mariadb/monto/my.cnf
 V_SVCDEPS	+= /var/lib/mariadb/monto /var/run/mariadb
 V_ARGS		+= --mount=type=bind,src=/var/lib/mariadb/monto,dst=/var/lib/mariadb
-V_ARGS		+= --mount=type=bind,src=/var/run/mariadb,dst=/var/run/mariadb
+V_ARGS		+= --mount=type=bind,src=/var/run/mariadb/monto,dst=/var/run/mariadb
 V_ARGS		+= --memory=128M
 V_ARGS		+= --publish=3307:3306/tcp
 V_ARGS 		+= codeberg.org/xvnet/mariadb:latest
@@ -16,4 +16,4 @@ V_TEMPLATE	= bash-tpl $(STATES_DIR)/services/mariadb/monto/my.cnf
 $(call end)
 
 $(call add-fs-directory,/var/lib/mariadb/monto)
-$(call add-fs-directory,/var/run/mariadb)
+$(call add-fs-directory,/var/run/mariadb/monto)
