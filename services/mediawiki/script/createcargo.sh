fe10720d5a9542bf22275aa27700f52926402e11
xtex <xtexchooser@duck.com>
2024-05-01 12:30:07 +0800

    MW: Add createcargo script

---
+++ services/mediawiki/script/createcargo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+runMW() {
+	podman exec -it mediawiki "$@"
+}
+
+(($# != 1)) && {
+	echo "Usage: atre s mediawiki createcargo <WIKI ID>" >&2
+	exit 1
+}
+echo "Creating cargo database for wiki $1"
+
+runMW php maintenance/run.php --wiki "$1" sql --query "CREATE DATABASE wikicargo$1"
+
+echo "Cargo database for wiki $1 initialized"
