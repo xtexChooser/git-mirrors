cb6bcc36a88072fe20c9193f1c2eb2e3241d0424
xtex <xtexchooser@duck.com>
2024-06-06 18:25:50 +0800

    Init vania

---
+++ vinia/build.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const pic = b.option(bool, "pic", "Produce Position Independent Code");
+
+    // The vinia module
+    const mod = b.addModule("vinia", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = false,
+        .link_libcpp = false,
+        // @TODO: SSP
+        // .stack_protector = true,
+        // .stack_check = true,
+        .pic = pic,
+    });
+
+    // The vinia library
+    const lib = b.addStaticLibrary(.{
+        .name = "vinia",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.root_module = mod.*;
+    b.installArtifact(lib);
+
+    // Bootloaders
+    switch (target.result.cpu.arch) {
+        .x86_64 => {
+            // Multiboot
+            const mb_exe = b.addExecutable(.{
+                .name = "vinia-multiboot",
+                .root_source_file = b.path("src/arch/x86_64/multiboot/main.zig"),
+                .target = target,
+                .optimize = optimize,
+            });
+            b.installArtifact(mb_exe);
+        },
+        else => unreachable,
+    }
+
+    // Unit tests
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path(""),
+        .target = target,
+        .optimize = optimize,
+    });
+    unit_tests.root_module = mod.*;
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+}
