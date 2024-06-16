5bfe6e627cd4647e9b14683fb02e57246dbe80c6
xtex <xtexchooser@duck.com>
2024-06-16 14:38:54 +0800

    Revert "vinia: x86: Allow bootloader wrapper to override bootstrap GDT table"
    This reverts commit 1344dfd79470338b4cb5dbb1c86a6a14cc33d221.

--- vinia/src/arch/x86/boot.zig
+++ vinia/src/arch/x86/boot.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const mem = std.mem;
 const Allocator = std.mem.Allocator;
 const log = std.log.scoped(.x86_boot);
-const is64 = @import("./root.zig").is64;
 const desc = @import("./desc.zig");
 
 
@@ -38,30 +37,27 @@ pub const GDT_ENTRY_NUM = 3;
 pub const GDT_ENTRY_DATA = 1;
 pub const GDT_ENTRY_CODE = 2;
 
-export const gdt_table = if (@hasDecl(@import("root"), "gdt_table"))
-    @import("root").gdt_table
-else
-    [GDT_ENTRY_NUM]desc.SegmentDescriptor{
-        undefined,
-        .{
-            .limit0 = 0xffff,
-            .base0 = 0,
-            .ty = .{ .data_seg = .{ .write = true } },
-            .priv_level = 0,
-            .limit1 = 0xf,
-            .long_mode_code = false,
-            .base2 = 0,
-        },
-        .{
-            .limit0 = 0xffff,
-            .base0 = 0,
-            .ty = .{ .code_seg = .{ .read = true } },
-            .priv_level = 0,
-            .limit1 = 0xf,
-            .long_mode_code = false,
-            .base2 = 0,
-        },
-    };
+export const gdt_table = [GDT_ENTRY_NUM]desc.SegmentDescriptor{
+    undefined,
+    .{
+        .limit0 = 0xffff,
+        .base0 = 0,
+        .ty = .{ .data_seg = .{ .write = true } },
+        .priv_level = 0,
+        .limit1 = 0xf,
+        .long_mode_code = false,
+        .base2 = 0,
+    },
+    .{
+        .limit0 = 0xffff,
+        .base0 = 0,
+        .ty = .{ .code_seg = .{ .read = true } },
+        .priv_level = 0,
+        .limit1 = 0xf,
+        .long_mode_code = false,
+        .base2 = 0,
+    },
+};
 
 pub fn load_gdt() void {
     desc.loadGdt(&gdt_table);
