f9753c502a4841552a87fdd2f9897c24358c2454
xtex <xtexchooser@duck.com>
2024-06-29 13:59:18 +0800

    atremis: Print digest diff

--- services/atremis/script/update-containers.sh
+++ services/atremis/script/update-containers.sh
@@ -6,11 +6,11 @@ declare -a servicesToRestart
 updated=""
 for img in $(podman image ls --format "{{if .RepoTags}}{{index .RepoTags 0}}{{end}}"); do
 	oldDigest="$(podman image ls "$img" --format "{{.Digest}}")"
-	podman pull --quiet "$img" || continue
+	podman pull --quiet "$img" >/dev/null || continue
 	newDigest="$(podman image ls "$img" --format "{{.Digest}}")"
 
 	if [[ "$oldDigest" != "$newDigest" ]]; then
-		echo Updated container image "$img"
+		echo "Updated image $img from $oldDigest to $newDigest"
 		updated=true
 		for svc in $(podman container ls --format "{{json .}}" | jq -s -r ".[] | select((.Labels[\"org.eu.xvnet.x.depimgs\"]? // \"\" | split(\",\") | contains([\"$img\"])) or (.Image == \"$img\")) | .Labels[\"org.eu.xvnet.x.dinitservice\"]? // \"\""); do
 			[[ "$svc" == "" ]] && continue
@@ -21,7 +21,7 @@ done
 
 if [[ -n "$updated" ]]; then
 	while IFS= read -r -d '' svc; do
-		echo Restarting container service "$svc"
+		echo "Restarting container service $svc"
 		dinitctl restart "$svc"
 	done < <(printf "%s\0" "${servicesToRestart[@]}" | sort -uz)
 
