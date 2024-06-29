9cf6022fe9e5ca803780847dfd5cb48836696471
xtex <xtexchooser@duck.com>
2024-06-29 23:04:39 +0800

    Revert "Merge commit 'a339dab9d65e8d6aa672ea61fa8e07824536d269'"
    This reverts commit 26599c59ec6d3ca75dd5e79f4d9c8137c501b61e, reversing
    changes made to 466ba20676f4ca8609bd26a53b5a0d2a5e59d6d6.

--- mw/extensions/DynamicPageList3/includes/Hooks.php
+++ mw/extensions/DynamicPageList3/includes/Hooks.php
@@ -219,11 +219,7 @@ class Hooks {
 
 		// we can remove the categories by save/restore
 		if ( $reset['categories'] ?? false ) {
-			$parserOutput = $parser->getOutput();
-			$saveCategories = array_combine(
-				$parserOutput->getCategoryNames(),
-				array_map( fn ( $value ) => $parserOutput->getCategorySortKey( $value ), $parserOutput->getCategoryNames() )
-			);
+			$saveCategories = $parser->getOutput()->getCategories();
 		}
 
 		// we can remove the images by save/restore
@@ -560,9 +556,9 @@ class Hooks {
 		if ( !self::$createdLinks['resetdone'] ) {
 			self::$createdLinks['resetdone'] = true;
 
-			foreach ( $parser->getOutput()->getCategoryNames() as $key ) {
+			foreach ( $parser->getOutput()->getCategories() as $key => $val ) {
 				if ( array_key_exists( $key, self::$fixedCategories ) ) {
-					self::$fixedCategories[$key] = $parser->getOutput()->getCategorySortKey( $key );
+					self::$fixedCategories[$key] = $val;
 				}
 			}
 
@@ -622,12 +618,7 @@ class Hooks {
 			}
 
 			if ( isset( self::$createdLinks ) && array_key_exists( 2, self::$createdLinks ) ) {
-				$parserOutput = $parser->getOutput();
-				$categories = array_combine(
-					$parserOutput->getCategoryNames(),
-					array_map( fn ( $value ) => $parserOutput->getCategorySortKey( $value ), $parserOutput->getCategoryNames() )
-				);
-				$parser->getOutput()->setCategories( array_diff_assoc( $categories, self::$createdLinks[2] ) );
+				$parser->getOutput()->setCategories( array_diff_assoc( $parser->getOutput()->getCategories(), self::$createdLinks[2] ) );
 			}
 
 			if ( isset( self::$createdLinks ) && array_key_exists( 3, self::$createdLinks ) ) {
