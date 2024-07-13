8f1df5739975379f02ca79e239e14a80ee40da05
xtex <xtexchooser@duck.com>
2024-07-13 17:40:58 +0800

    Add licenses window

---
+++ src/assets.rs
@@ -0,0 +1,55 @@
+use std::{fs, io::Read, path::PathBuf};
+
+use anyhow::Result;
+use egui::{FontData, FontFamily};
+
+pub const CUBIC11_FONT: &[u8] = include_bytes!("assets/Cubic_11_1.300_R.ttf.xz");
+pub const CUBIC11_LICENSE: &str = include_str!("assets/Cubic_11-LICENSE.txt");
+
+pub const LICENSE_STR: &str = include_str!("../LICENSE");
+
+pub fn configure_fonts(ctx: &eframe::egui::Context) -> Result<()> {
+    ctx.set_zoom_factor(1.2);
+    let mut fonts = egui::FontDefinitions::default();
+    {
+        // load msyh
+        let path = PathBuf::from(std::env::var("SystemRoot")?).join("Fonts/msyh.ttc");
+        if path.exists() {
+            fonts
+                .font_data
+                .insert("msyh".to_owned(), FontData::from_owned(fs::read(path)?));
+            fonts
+                .families
+                .get_mut(&FontFamily::Proportional)
+                .unwrap()
+                .insert(0, "msyh".to_owned());
+            fonts
+                .families
+                .get_mut(&FontFamily::Monospace)
+                .unwrap()
+                .push("msyh".to_owned());
+        }
+    }
+
+    {
+        // load Cubic-11
+        let mut buf = Vec::new();
+        xz2::read::XzDecoder::new(&CUBIC11_FONT[..]).read_to_end(&mut buf)?;
+        fonts
+            .font_data
+            .insert("cubic11".to_owned(), FontData::from_owned(buf));
+        fonts
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "cubic11".to_owned());
+        fonts
+            .families
+            .get_mut(&FontFamily::Monospace)
+            .unwrap()
+            .insert(0, "cubic11".to_owned());
+    }
+
+    ctx.set_fonts(fonts);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #![windows_subsystem = "windows"]
-
-use std::{fs, io::Read, path::PathBuf};
-
 use anyhow::Result;
-use egui::{FontData, FontFamily};
+
+mod assets;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -22,59 +20,16 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-struct MainApp {}
+struct MainApp {
+    show_licenses: bool,
+}
 
 impl MainApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Result<Self> {
-        Self::configure_fonts(&cc.egui_ctx)?;
-        Ok(Self {})
-    }
-
-    pub fn configure_fonts(ctx: &eframe::egui::Context) -> Result<()> {
-        ctx.set_zoom_factor(1.2);
-        let mut fonts = egui::FontDefinitions::default();
-        {
-            // load msyh
-            let path = PathBuf::from(std::env::var("SystemRoot")?).join("Fonts/msyh.ttc");
-            if path.exists() {
-                fonts
-                    .font_data
-                    .insert("msyh".to_owned(), FontData::from_owned(fs::read(path)?));
-                fonts
-                    .families
-                    .get_mut(&FontFamily::Proportional)
-                    .unwrap()
-                    .insert(0, "msyh".to_owned());
-                fonts
-                    .families
-                    .get_mut(&FontFamily::Monospace)
-                    .unwrap()
-                    .push("msyh".to_owned());
-            }
-        }
-
-        {
-            // load Cubic-11
-            let mut buf = Vec::new();
-            xz2::read::XzDecoder::new(&include_bytes!("assets/Cubic_11_1.300_R.ttf.xz")[..])
-                .read_to_end(&mut buf)?;
-            fonts
-                .font_data
-                .insert("cubic11".to_owned(), FontData::from_owned(buf));
-            fonts
-                .families
-                .get_mut(&FontFamily::Proportional)
-                .unwrap()
-                .insert(0, "cubic11".to_owned());
-            fonts
-                .families
-                .get_mut(&FontFamily::Monospace)
-                .unwrap()
-                .insert(0, "cubic11".to_owned());
-        }
-
-        ctx.set_fonts(fonts);
-        Ok(())
+        assets::configure_fonts(&cc.egui_ctx)?;
+        Ok(Self {
+            show_licenses: false,
+        })
     }
 }
 
@@ -82,21 +37,29 @@ impl eframe::App for MainApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading(format!("YJYZ Tools - {}", env!("CARGO_PKG_VERSION")));
-            if ui.link("源代码").clicked() {
-                ctx.open_url(egui::OpenUrl::new_tab(
-                    "https://codeberg.org/xtex/yjyz-tools",
-                ));
-            }
+            ui.horizontal(|ui| {
+                if ui.link("源代码").clicked() {
+                    ctx.open_url(egui::OpenUrl::new_tab(
+                        "https://codeberg.org/xtex/yjyz-tools",
+                    ));
+                }
+                if ui.link("开源许可证").clicked() {
+                    self.show_licenses = true;
+                }
+            });
 
             egui::menu::bar(ui, |ui| {
                 if ui.button("Open").clicked() {
                     // …
                 }
             });
-            egui::Window::new("My Window")
-                .fade_in(false)
+            egui::Window::new("开源许可证")
+                .open(&mut self.show_licenses)
+                .vscroll(true)
                 .show(ctx, |ui| {
-                    ui.label("Hello World!");
+                    ui.label(assets::LICENSE_STR);
+                    ui.heading("Cubic-11");
+                    ui.label(assets::CUBIC11_LICENSE);
                 });
         });
     }
