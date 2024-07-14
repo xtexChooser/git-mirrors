bdb3e579343fcc022a3f2b59d7257dfab3bf3743
xtex <xtexchooser@duck.com>
2024-07-14 17:11:40 +0800

    Remove i686 build

--- maint/publish.sh
+++ maint/publish.sh
@@ -2,7 +2,6 @@
 set -E
 
 version="$(grep -E '^version = "(.*)"$' Cargo.toml | head -n1 | tail -c+12 | head -c-2)"
-cargo build --release --target i686-pc-windows-gnu
 cargo build --release --target x86_64-pc-windows-gnu
 git tag "$version"
 git push --follow-tags
