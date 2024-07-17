3e983993b50efe8b3f32891fcdc698122564d905
xtex <xtexchooser@duck.com>
2024-07-17 11:11:58 +0800

    dinit: Move DINITCTL_DEPS to order-only

--- modules/dinit.mk
+++ modules/dinit.mk
@@ -13,13 +13,13 @@ $(call mktrace, Define dinit service target: $(V_SERVICE))
 $(call mktrace-vars,$(DINIT_SERVICE_VARS))
 $(call apply-target,$(V_TARGET_NAME))
 $(call vt-target,$(V_TARGET_NAME))
-$(V_TARGET_NAME): $(DINITCTL_DEPS) $(v-deps) \
+$(V_TARGET_NAME): $(v-deps) \
 		$(if $(V_SYSTEM)$(call streq,root,$(USER)),$(call file-imp-dep,$(DINITD_DIR)/$(V_SERVICE)) \
 		$(call file-imp-dep,/lib/dinit.d/$(V_SERVICE)) \
 		$(call file-imp-dep,/run/dinit.d/$(V_SERVICE)) \
 		$(call file-imp-dep,/usr/local/lib/dinit.d/$(V_SERVICE)), \
 		$(call file-imp-dep,$(DINITD_USER_DIR)/$(V_SERVICE))) \
-		| $(v-deps-order)
+		| $(v-deps-order) $(DINITCTL_DEPS)
 	export E_MAJOR=dinit E_SERVICE=$(V_SERVICE)
 $(if $(call is-true,$(V_RUNNING)),
 	if ! $(V_DINITCTL) is-started $(V_SERVICE) $(DROP_STDOUT); then
@@ -42,18 +42,18 @@ endef
 $(call define-func, dinit-service)
 
 $(call vt-target, dinit-start dinit-stop dinit-restart dinit-reload dinit-shutdown)
-dinit-start: $(DINITCTL_DEPS)
+dinit-start: | $(DINITCTL_DEPS)
 	$(DINITCTL) start $(E_SERVICE)
 	$(call succ, Started dinit service $(E_SERVICE))
-dinit-stop: $(DINITCTL_DEPS)
+dinit-stop: | $(DINITCTL_DEPS)
 	$(DINITCTL) stop $(E_SERVICE)
 	$(call succ, Stopped dinit service $(E_SERVICE))
-dinit-restart: $(DINITCTL_DEPS)
+dinit-restart: | $(DINITCTL_DEPS)
 	$(DINITCTL) restart $(E_SERVICE)
 	$(call succ, Restarted dinit service $(E_SERVICE))
-dinit-reload: $(DINITCTL_DEPS)
+dinit-reload: | $(DINITCTL_DEPS)
 	$(DINITCTL) reload $(E_SERVICE)
 	$(call succ, Reloaded dinit service $(E_SERVICE))
-dinit-shutdown: $(DINITCTL_DEPS)
+dinit-shutdown: | $(DINITCTL_DEPS)
 	$(DINITCTL) shutdown
 	$(call succ, Shutting down dinit daemon)
