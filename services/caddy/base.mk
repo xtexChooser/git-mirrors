58185f388b4dafdb71891893a48834fdbe8dba8c
xtex <xtexchooser@duck.com>
2024-07-17 11:14:35 +0800

    caddy: Create /var/lib/caddy

--- services/caddy/base.mk
+++ services/caddy/base.mk
@@ -1,6 +1,6 @@
 $(call x-container-service)
 V_SERVICE	= caddy
-V_DEPS		+= /etc/caddy/Caddyfile
+V_DEPS		+= /etc/caddy/Caddyfile /var/lib/caddy
 V_ARGS		+= --cap-add=CAP_NET_BIND_SERVICE
 V_ARGS		+= --env HOME=/root
 V_ARGS		+= --mount=type=bind,src=/etc/caddy,dst=/etc/caddy,ro=true
@@ -12,6 +12,8 @@ V_ARGS		+= --memory=64M
 $(call invoke-hooks,caddy-container-opts)
 V_ARGS 		+= codeberg.org/xvnet/x-caddy
 $(call end)
+
+$(call add-fs-directory,/var/lib/caddy)
 
 CADDY_INCLUDES :=
 $(call fs-file)
