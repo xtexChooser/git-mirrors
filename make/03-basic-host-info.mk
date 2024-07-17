ad35b946f74802a0e2391659c439f83ca5771790
xtex <xtexchooser@duck.com>
2024-07-17 10:36:39 +0800

    Make inetutils optional

--- make/03-basic-host-info.mk
+++ make/03-basic-host-info.mk
@@ -1,5 +1,5 @@
 HOST_USER ?= $(USER)
-$(if $(call not,$(HOSTNAME)),$(eval HOSTNAME := $(shell hostname)))
+$(if $(call not,$(HOSTNAME)),$(eval HOSTNAME := $(shell hostname 2>/dev/null || cat /etc/hostname 2>/dev/null)))
 HOST_KERNEL ?= $(shell uname -a)
 
 HOME := $(abspath $(HOME))
