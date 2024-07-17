180895551946c32a38f5c57c58c5a6e3e63026f7
xtex <xtexchooser@duck.com>
2024-07-17 11:04:31 +0800

    Rename V_DEP_ORD to V_DEPS_ORD

--- make/02-v-deps.mk
+++ make/02-v-deps.mk
@@ -1,11 +1,11 @@
-v-deps-var := V_DEPS V_DEP_VARS V_DEP_ORD V_DEP_ORDV
+v-deps-var := V_DEPS V_DEP_VARS V_DEPS_ORD V_DEP_ORDV
 
 define v-deps
 $(V_DEPS) $(call dep-vars,$(V_DEP_VARS))
 endef
 
 define v-deps-order
-$(V_DEP_ORD) $(call dep-vars,$(V_DEP_ORDV))
+$(V_DEPS_ORD) $(call dep-vars,$(V_DEP_ORDV))
 endef
 
 define v-var-dep-files
