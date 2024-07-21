26433f356291ae5986a929c9816b830a5becdd53
xtex <xtexchooser@duck.com>
2024-07-21 16:58:51 +0800

    Update version

--- tm/misc/pgdoc-title-shortener.js
+++ tm/misc/pgdoc-title-shortener.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PostgreSQL Doc Title Shortener
 // @namespace    https://xtexx.eu.org/
-// @version      2024-07-21
+// @version      0.1.1
 // @description  Make the title of PostgreSQL Documentation shorter!
 // @author       xtex
 // @match        https://www.postgresql.org/docs/*
