41d41afe2029bf55ff551f79ce22525d1dbdb9db
xtex <xtexchooser@duck.com>
2024-07-27 17:44:34 +0800

    Fix modrinth upload

--- scripts/upload-to-mr.sh
+++ scripts/upload-to-mr.sh
@@ -8,7 +8,7 @@ echo "$version"
 payload=$(jo -- project_id=HsMwyVxf \
     file_parts="$(jo -a file)" featured=false loaders="$(jo -a fabric)" \
     version_type=release \
-    game_versions="$(jo -a "$(grep '^minecraft = "' pack.toml | sed -e 's/minecraft = "//' | sed -e 's/"//')")" \
+    game_versions="$(jo -a -- -s "$(grep '^minecraft = "' pack.toml | sed -e 's/minecraft = "//' | sed -e 's/"//')")" \
     dependencies="$(jo -a < /dev/null)" \
     -s "version_number=$version" -s "name=$version" \
     "changelog=@$CHANGELOG")
