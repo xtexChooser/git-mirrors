1fadd7b2f12121cedb291b74ba029ed48f4cc5aa
Jon Robson <jdlrobson@gmail.com>
2024-07-31 08:24:30 -0700

    Improves error handling
    * Outputs localized message
    * Handles issues with shell script
    
    Change-Id: I4c0ccc3ecd43dd1a6cf08191b0697a5b299adb84

--- src/ParserFunction.php
+++ src/ParserFunction.php
@@ -8,6 +8,17 @@ use MediaWiki\Shell\Shell;
 
 class ParserFunction {
 	/**
+	 * Renders an error
+	 *
+	 * @param string $errMsg
+	 * @return array
+	 */
+	private function renderError( string $errMsg ) {
+		$error = Html::errorBox( $errMsg );
+		return [ $error, 'noparse' => true, 'isHTML' => true ];
+	}
+
+	/**
 	 * Entry point for the `{{#chart:}}` parser function.
 	 *
 	 * @param Parser $parser
@@ -15,9 +26,7 @@ class ParserFunction {
 	 */
 	public function render( Parser $parser ) {
 		if ( Shell::isDisabled() ) {
-			// TODO i18n
-			$error = Html::errorBox( 'Charts cannot be rendered because shell execution is disabled' );
-			return [ $error, 'noparse' => true, 'isHTML' => true ];
+			return $this->renderError( $parser->msg( 'chart-error-shell-disabled' )->text() );
 		}
 		$dir = dirname( __DIR__ ) . '/cli';
 		$result = Shell::command(
@@ -30,8 +39,21 @@ class ParserFunction {
 		)
 			->workingDirectory( dirname( __DIR__ ) . '/cli' )
 			->execute();
-		// TODO check exit status for error
+		$error = $result->getStderr();
 		$svg = $result->getStdout();
+		if ( $error ) {
+			return $this->renderError(
+				$parser->msg( 'chart-error-shell-error' )->text() .
+				// TODO don't expose this in the HTML output, instead log to Logstash
+				// and output a reqid that refers to it
+				Html::element( 'blockquote', [ 'style' => 'display: none' ], $error )
+			);
+		}
+		// If no SVG was returned, then the shell command didn't return anything, so treat this
+		// as an error.
+		if ( !trim( $svg ) ) {
+			return $this->renderError( $parser->msg( 'chart-error-shell-no-response' )->text() );
+		}
 		// HACK work around a parser bug that inserts <p> tags even though we said not to parse
 		$svg = str_replace( ">\n", '>', $svg );
 		// Phan complains that we're outputting HTML that came from a shell command -- but we trust
