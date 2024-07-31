6105879099ac2fe447bdd57e76086b3fb5fd87e2
Katie Filbert <aude.wiki@gmail.com>
2024-07-31 17:28:14 -0400

    Improve error handling in chart parser function
    Change-Id: Icf11fd3ab69b548db3e1a175fcb5275a46bb0ea8

--- src/ParserFunction.php
+++ src/ParserFunction.php
@@ -4,9 +4,11 @@ namespace MediaWiki\Extension\Chart;
 
 use JsonConfig\JCTabularContent;
 use MediaWiki\Html\Html;
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Parser\Parser;
 use MediaWiki\Shell\Shell;
+use MediaWiki\Status\Status;
 use MediaWiki\Title\Title;
 use WikiPage;
 
@@ -30,6 +32,10 @@ class ParserFunction {
 	 * @return array
 	 */
 	public function render( Parser $parser, ...$args ) {
+		$logger = LoggerFactory::getInstance( 'Chart' );
+
+		// @todo incrementExpensiveFunctionCount
+
 		if ( Shell::isDisabled() ) {
 			return $this->renderError( $parser->msg( 'chart-error-shell-disabled' )->text() );
 		}
@@ -68,7 +74,7 @@ class ParserFunction {
 				$formatData = $formatContent->getLocalizedData( $parser->getTargetLanguage() );
 			}
 		} else {
-			var_dump( 'no format' );
+			return $this->renderError( $parser->msg( 'chart-error-chart-definition-not-found' )->text() );
 		}
 
 		$sourceData = (object)[ 'fields' => [], 'data' => [] ];
@@ -81,10 +87,10 @@ class ParserFunction {
 				//$sourceContent = JCSingleton::getContent( $source->getTitleValue() );
 				$sourceData = $sourceContent->getLocalizedData( $parser->getTargetLanguage() );
 			} else {
-				var_dump( 'incomptable data source' );
+				return $this->renderError( $parser->msg( 'chart-error-incompatible-data-source' )->text() );
 			}
 		} else {
-			var_dump( 'no data' );
+			return $this->renderError( $parser->msg( 'chart-error-data-source-page-not-found' )->text() );
 		}
 
 		$chartPath = tempnam( \wfTempDir(), 'chart-json' );
@@ -92,7 +98,6 @@ class ParserFunction {
 		$sourcePath = tempnam( \wfTempDir(), 'data-json' );
 		file_put_contents( $sourcePath, json_encode( $sourceData ) );
 
-		$dir = dirname( __DIR__ ) . '/cli';
 		$result = Shell::command(
 			'node',
 			'./dist/index.js',
@@ -103,28 +108,35 @@ class ParserFunction {
 		)
 			->workingDirectory( dirname( __DIR__ ) . '/cli' )
 			->execute();
+
+		$error = $result->getStderr();
+		if ( $error ) {
+			$logger->warning( 'Chart shell command failed to generate output', [
+				'error' => $error
+			] );
+
+			// @todo tracking category
+			$status = Status::newFatal( 'chart-error-shell-error' );
+		} else {
+			$svg = $result->getStdout();
+
+			// HACK work around a parser bug that inserts <p> tags even though we said not to parse
+			$svg = str_replace( ">\n", '>', $svg );
+
+			// Phan complains that we're outputting HTML that came from a shell command -- but we trust
+			// our own shell script
+			$chartOutput = [ "<div class=\"mw-chart\">$svg</div>", 'noparse' => true, 'isHTML' => true ];
+			$status = Status::newGood( $chartOutput );
+		}
+
 		unlink( $chartPath );
 		unlink( $sourcePath );
-		$error = $result->getStderr();
-		$svg = $result->getStdout();
-		if ( $error ) {
-			return $this->renderError(
-				$parser->msg( 'chart-error-shell-error' )->text() .
-				// TODO don't expose this in the HTML output, instead log to Logstash
-				// and output a reqid that refers to it
-				Html::element( 'blockquote', [ 'style' => 'display: none' ], $error )
-			);
+
+		if ( !$status->isGood() ) {
+			// If no SVG was returned, then the Shell command didn't return anything so treat this an error
+			return $this->renderError( $parser->msg( 'chart-error-shell-error' )->text() );
 		}
-		// If no SVG was returned, then the shell command didn't return anything, so treat this
-		// as an error.
-		if ( !trim( $svg ) ) {
-			return $this->renderError( $parser->msg( 'chart-error-shell-no-response' )->text() );
-		}
-		// HACK work around a parser bug that inserts <p> tags even though we said not to parse
-		$svg = str_replace( ">\n", '>', $svg );
-		// Phan complains that we're outputting HTML that came from a shell command -- but we trust
-		// our own shell script
-		// @phan-suppress-next-line SecurityCheck-XSS
-		return [ "<div class=\"mw-chart\">$svg</div>", 'noparse' => true, 'isHTML' => true ];
+
+		return $status->getValue();
 	}
 }
