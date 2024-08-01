648d6068028db50d85db4fcc00e425257e4290e1
Jon Robson <jdlrobson@gmail.com>
2024-08-01 14:07:55 -0700

    Test for unique ID
    Bug: T371558
    Change-Id: Id586b736edf9dde273e62eae908adc742fbb0353

--- tests/jest/renderChart.test.js
+++ tests/jest/renderChart.test.js
@@ -15,4 +15,22 @@ describe( 'renderChart', () => {
 		fs.unlinkSync( output );
 		expect( result ).toMatchSnapshot();
 	} );
+
+	test( 'renders a graph with unique id', async () => {
+		const output = './result.json';
+		const dataPath = `${ __dirname }/../../cli/chart-data.json`;
+		const copyPath = `${ __dirname }/../../cli/chart-definition-copy.json`;
+		const definition = JSON.parse( fs.readFileSync( `${ __dirname }/../../cli/chart-definition.json`, { encoding: 'utf-8' } ) );
+		definition.idPrefix = 'unique-';
+		fs.writeFileSync( copyPath, JSON.stringify( definition ) );
+		await renderChart(
+			dataPath,
+			`${ __dirname }/../../cli/chart-definition-copy.json`,
+			output
+		);
+		const result = fs.readFileSync( output ).toString();
+		fs.unlinkSync( output );
+		fs.unlinkSync( copyPath );
+		expect( result ).not.toContain( 'id="zr2-c2"' );
+	} );
 } );
