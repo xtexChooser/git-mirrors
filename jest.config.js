1a5db5197b1c06685d321e28b24ea7f769e5a1ce
Jon Robson <jdlrobson@gmail.com>
2024-08-02 08:36:03 -0700

    Restore tests
    This reverts commit 525aef30e86335d6b33f319ccd6ca0a8ae7ed4e5.
    It also fixes dependencies to a required Node version
    
    Change-Id: I11bb674c5433958a01c3e754d414e755e7cea74f

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,10 @@ module.exports = {
 	// Automatically clear mock calls and instances between every test
 	clearMocks: true,
 
+	moduleNameMapper: {
+		'(.+)\\.js': '$1'
+	},
+
 	// Indicates whether the coverage information should be collected while executing the test
 	collectCoverage: true,
 
