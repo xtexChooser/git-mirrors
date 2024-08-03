bfc10c333a8b88f37c8bdb22ff9053f7d5797218
Katie Filbert <aude.wiki@gmail.com>
2024-08-03 16:34:34 -0400

    Support showSymbols chart definition option
    - Also updated sample test data, as the MediaWiki side handles
      localisation and data for all the languages is not sent
      to to the cli.
    - Add tests for showSymbols (not specified, set to true or invalid)
    
    Bug: T371758
    Change-Id: I3a3425f4f397c3fa38349fb56d44828676cb7a6c

--- tests/jest/renderChart.test.js
+++ tests/jest/renderChart.test.js
@@ -7,8 +7,8 @@ describe( 'renderChart', () => {
 	test( 'renders a graph', async () => {
 		const output = './result.json';
 		await renderChart(
-			`${ __dirname }/../../cli/chart-data.json`,
-			`${ __dirname }/../../cli/chart-definition.json`,
+			`${ __dirname }/../../cli/data/1993_Canadian_federal_election/data.json`,
+			`${ __dirname }/../../cli/data/1993_Canadian_federal_election/chart.json`,
 			output
 		);
 		const result = fs.readFileSync( output ).toString();
@@ -18,19 +18,72 @@ describe( 'renderChart', () => {
 
 	test( 'renders a graph with unique id', async () => {
 		const output = './result.json';
-		const dataPath = `${ __dirname }/../../cli/chart-data.json`;
-		const copyPath = `${ __dirname }/../../cli/chart-definition-copy.json`;
-		const definition = JSON.parse( fs.readFileSync( `${ __dirname }/../../cli/chart-definition.json`, { encoding: 'utf-8' } ) );
+		const dataPath = `${ __dirname }/../../cli/data/1993_Canadian_federal_election/data.json`;
+		const copyPath = `${ __dirname }/../../cli/data/chart-definition-copy.json`;
+		const definition = JSON.parse(
+			fs.readFileSync(
+				`${ __dirname }/../../cli/data/1993_Canadian_federal_election/chart.json`,
+				{ encoding: 'utf-8' }
+			)
+		);
 		definition.idPrefix = 'unique-';
 		fs.writeFileSync( copyPath, JSON.stringify( definition ) );
 		await renderChart(
 			dataPath,
-			`${ __dirname }/../../cli/chart-definition-copy.json`,
+			`${ __dirname }/../../cli/data/chart-definition-copy.json`,
 			output
 		);
 		const result = fs.readFileSync( output ).toString();
 		fs.unlinkSync( output );
 		fs.unlinkSync( copyPath );
 		expect( result ).not.toContain( 'id="zr2-c2"' );
+	} );
+
+	test( 'render a line chart with no showSymbols', async () => {
+		const output = './result.json';
+		const chartDataPath = `${ __dirname }/../../cli/data/1993_Canadian_federal_election/data.json`;
+		const chartDefinitionPath = `${ __dirname }/../../cli/data/1993_Canadian_federal_election/chart.json`;
+
+		const definition = JSON.parse(
+			fs.readFileSync( chartDefinitionPath, { encoding: 'utf-8' } )
+		);
+
+		delete definition.showSymbols;
+
+		const tmpChartDefinitionPath = `${ __dirname }/../../cli/data/chart-definition-no-symbols.json`;
+		fs.writeFileSync( tmpChartDefinitionPath, JSON.stringify( definition ) );
+
+		await renderChart( chartDataPath, tmpChartDefinitionPath, output );
+
+		const result = fs.readFileSync( output ).toString();
+
+		fs.unlinkSync( output );
+		fs.unlinkSync( tmpChartDefinitionPath );
+
+		expect( result ).toMatchSnapshot();
+	} );
+
+	test( 'render a line chart with invalid showSymbols', async () => {
+		const output = './result.json';
+		const chartDataPath = `${ __dirname }/../../cli/data/1993_Canadian_federal_election/data.json`;
+		const chartDefinitionPath = `${ __dirname }/../../cli/data/1993_Canadian_federal_election/chart.json`;
+
+		const definition = JSON.parse(
+			fs.readFileSync( chartDefinitionPath, { encoding: 'utf-8' } )
+		);
+
+		definition.showSymbols = 'kittens';
+
+		const tmpChartDefinitionPath = `${ __dirname }/../../cli/data/chart-definition-no-symbols.json`;
+		fs.writeFileSync( tmpChartDefinitionPath, JSON.stringify( definition ) );
+
+		await renderChart( chartDataPath, tmpChartDefinitionPath, output );
+
+		const result = fs.readFileSync( output ).toString();
+
+		fs.unlinkSync( output );
+		fs.unlinkSync( tmpChartDefinitionPath );
+
+		expect( result ).toMatchSnapshot();
 	} );
 } );
