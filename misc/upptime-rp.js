04b5730bd6191ff8e9656319342ff1371289fb88
xtex <xtexchooser@duck.com>
2024-09-15 22:51:17 +0800

    Fix

--- misc/upptime-rp.js
+++ misc/upptime-rp.js
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: Unlicense
 
+// Usage: add the following configuration to .upptimerc.yml
+// ```yaml
+// status-website:
+//   scripts:
+//     - src: https://xtex.codeberg.page/gadgets/@main/misc/upptime-rp.js
+// ```
+
 (function () {
 	var originalFetch = fetch;
 	window.fetch = (input, init) => originalFetch('https://rp.chitang.dev/' + input, init);
-}();
+})();
