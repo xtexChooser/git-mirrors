af7ef6830efecbac0aaf55c777ad00ac57711f58
xtex <xtexchooser@duck.com>
2024-09-15 23:04:16 +0800

    Fix

--- misc/upptime-rp.js
+++ misc/upptime-rp.js
@@ -8,6 +8,29 @@
 // ```
 
 (function () {
-	var originalFetch = fetch;
-	window.fetch = (input, init) => originalFetch('https://rp.chitang.dev/' + input, init);
+	const RP = 'https://rp.chitang.dev/';
+
+	var fetch0 = fetch;
+	window.fetch = (input, init) => fetch0(RP + input, init);
+
+	const observer = new MutationObserver((mutationList, observer) => {
+		/**
+		 * @param {HTMLElement} node
+		 */
+		function patchNode(node) {
+			const src = node.getAttribute('src');
+			if (src && !src.startsWith(RP))
+				node.setAttribute('src', RP + src);
+
+			const style = node.getAttribute('style');
+			if (style && style.includes('https://'))
+				node.setAttribute('style', style.replace('https://', RP + 'https://'));
+		}
+		for (const mutation of mutationList) {
+			patchNode(node.target);
+			for (const node of mutation.addedNodes)
+				patchNode(node);
+		}
+	});
+	observer.observe(document.getElementById("sapper"), { subtree: true, attributes: false, childList: true });
 })();
