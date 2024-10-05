caac0ace5967d60aa8855efbeff9a388cf20e6e2
xtex <xtexchooser@duck.com>
2024-10-05 21:07:29 +0800

    Revert "Add an option to hide name / email input boxes"
    This reverts commit f08522c48ecf184809ec4e05ace4eb5a3dcac31e.

--- mw/extensions/ContactPage/includes/SpecialContact.php
+++ mw/extensions/ContactPage/includes/SpecialContact.php
@@ -222,44 +222,33 @@ class SpecialContact extends UnlistedSpecialPage {
 
 		$additional = $config['AdditionalFields'] ?? [];
 
-		$formItems = [];
-		if ( !$config['HideDetails'] ) {
-			$formItems += [
-				'FromName' => [
-					'label-message' => $this->getFormSpecificMessageKey( 'contactpage-fromname' ),
-					'type' => 'text',
-					'required' => $config['RequireDetails'],
-					'default' => $fromName,
-					'disabled' => $nameReadonly,
-				],
-				'FromAddress' => [
-					'label-message' => $this->getFormSpecificMessageKey( 'contactpage-fromaddress' ),
-					'type' => 'email',
-					'required' => $config['RequireDetails'],
-					'default' => $fromAddress,
-					'disabled' => $emailReadonly,
-				]
-			];
-			if ( !$config['RequireDetails'] ) {
-				$formItems['FromInfo'] = [
-					'label' => '',
-					'type' => 'info',
-					'default' => Html::rawElement(
-						'small',
-						[],
-						$this->msg(
-							$this->getFormSpecificMessageKey( 'contactpage-formfootnotes' )
-						)->escaped()
-					),
-					'raw' => true,
-				];
-			}
-			$additional += [
-				'CCme' => [
-					'label-message' => $this->getFormSpecificMessageKey( 'emailccme' ),
-					'type' => 'check',
-					'default' => $this->userOptionsLookup->getBoolOption( $user, 'ccmeonemails' ),
-				]
+		$formItems = [
+			'FromName' => [
+				'label-message' => $this->getFormSpecificMessageKey( 'contactpage-fromname' ),
+				'type' => 'text',
+				'required' => $config['RequireDetails'],
+				'default' => $fromName,
+				'disabled' => $nameReadonly,
+			],
+			'FromAddress' => [
+				'label-message' => $this->getFormSpecificMessageKey( 'contactpage-fromaddress' ),
+				'type' => 'email',
+				'required' => $config['RequireDetails'],
+				'default' => $fromAddress,
+				'disabled' => $emailReadonly,
+			]
+		];
+
+		if ( !$config['RequireDetails'] ) {
+			$formItems['FromInfo'] = [
+				'label' => '',
+				'type' => 'info',
+				'default' => Html::rawElement( 'small', [],
+					$this->msg(
+						$this->getFormSpecificMessageKey( 'contactpage-formfootnotes' )
+					)->escaped()
+				),
+				'raw' => true,
 			];
 		}
 
@@ -271,6 +260,11 @@ class SpecialContact extends UnlistedSpecialPage {
 				'disabled' => $subjectReadonly,
 			],
 		] + $additional + [
+			'CCme' => [
+				'label-message' => $this->getFormSpecificMessageKey( 'emailccme' ),
+				'type' => 'check',
+				'default' => $this->userOptionsLookup->getBoolOption( $this->getUser(), 'ccmeonemails' ),
+			],
 			'FormType' => [
 				'class' => 'HTMLHiddenField',
 				'label' => 'Type',
@@ -382,8 +376,8 @@ class SpecialContact extends UnlistedSpecialPage {
 		// Initialize the sender to the site address
 		$senderAddress = $siteAddress;
 
-		$fromAddress = $formData['FromAddress'] ?? null;
-		$fromName = $formData['FromName'] ?? null;
+		$fromAddress = $formData['FromAddress'];
+		$fromName = $formData['FromName'];
 
 		$fromUserAddress = null;
 		$replyTo = null;
@@ -539,7 +533,7 @@ class SpecialContact extends UnlistedSpecialPage {
 
 		// if the user requested a copy of this mail, do this now,
 		// unless they are emailing themselves, in which case one copy of the message is sufficient.
-		if ( isset( $formData['CCme'] ) && $formData['CCme'] && $fromUserAddress ) {
+		if ( $formData['CCme'] && $fromUserAddress ) {
 			$cc_subject = $this->msg( 'emailccsubject', $contactRecipientUser->getName(), $subject )->text();
 			if ( $hookRunner->onContactForm(
 				$fromUserAddress, $senderAddress, $cc_subject, $text, $this->formType, $formData )
