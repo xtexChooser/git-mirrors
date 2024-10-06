da959dc4b7b1b4d56e86067a60ee3ad4b26e3da1
xtex <xtexchooser@duck.com>
2024-10-06 21:37:47 +0800

    Update publish script

--- maint/publish.sh
+++ maint/publish.sh
@@ -2,6 +2,19 @@
 set -E
 
 version="$(grep -E '^version = "(.*)"$' Cargo.toml | head -n1 | tail -c+12 | head -c-2)"
+
+echo "Setting version to $version ..."
+yq -i -I 4 ".version |= \"$version\"" maint/version.json
+
+echo "Building x86-64 prebuilt binary ..."
 cargo build --release --target x86_64-pc-windows-gnu
-git tag "$version"
-git push --follow-tags
+
+echo "Updating checksums ..."
+checksum="$(sha256sum target/x86_64-pc-windows-gnu/release/yjyz-tools.exe | cut -d' ' -f3)"
+yq -i -I 4 ".checksum |= \"$checksum\"" maint/version.json
+
+echo "Sending files ..."
+rsync -v maint/version.json \
+    envs.net:public_html/yjyz-tools/version_v1.json
+rsync -v target/x86_64-pc-windows-gnu/release/yjyz-tools.exe \
+    envs.net:public_html/yjyz-tools/yzt-prebuilt.exe
