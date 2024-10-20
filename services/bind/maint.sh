bbface70b1cd9c95fc165677df836201dfbf02c2
xtex <xtexchooser@duck.com>
2024-10-20 09:55:28 +0800

    BIND: Rewrite maintenance scripts in maint.sh

---
+++ services/bind/maint.sh
@@ -0,0 +1,34 @@
+# shellcheck shell=bash
+
+atre::bind::validate() {
+	make HOSTNAME=opilio.s.xvnet0.eu.org USER=root LEONIS_LOAD_ALL=y do-tpl TPL_BACKEND=bash-tpl \
+		TPL_IN=services/bind/conf/named.conf TPL_OUT=.bind-valid.conf
+
+	podman image exists codeberg.org/xens/dns-root-zone:latest ||
+		podman image pull codeberg.org/xens/dns-root-zone:latest
+
+	(
+		while true; do
+			if [[ "$(podman container inspect bind-validate 2>/dev/null | jq '.[0].State.Status' -r)" == 'running' ]]; then
+				podman container rm --force bind-validate >/dev/null
+				break
+			fi
+		done
+	) &
+
+	podman run -it --rm --name bind-validate -v "$(pwd)":/validate \
+		--mount=type=image,source=codeberg.org/xens/dns-root-zone:latest,destination=/opt/root-zone \
+		codeberg.org/xens/bind:latest \
+		named -c /validate/.bind-valid.conf -g | tee .bind-valid.log
+	succ=true
+	if grep -F 'exiting (due to fatal error)' .bind-valid.log; then
+		succ=''
+	fi
+	rm -f .bind-valid.log
+	if [[ -z "$succ" ]]; then
+		echo 'Validation failed' >&2
+		return 1
+	else
+		rm -f .bind-valid.conf
+	fi
+}
