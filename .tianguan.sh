d5461301a28cd7bcc9bd51d20baed0440253b2e0
xtex <xtexchooser@duck.com>
2024-11-15 23:02:36 +0800

    tianguan: Add -pull and -upd

--- .tianguan.sh
+++ .tianguan.sh
@@ -8,12 +8,20 @@ done <<<"$(grep -Eh '^### tiang::' hosts/*.mk)"
 tiang::defineCommand atremis::syncsec
 tiangCommandsUsage+="""
     -ss --syncsec  [FILE]       Copy a secret file to targets
+	-pull                       Run atremis pull
+	-upd                        Run atremis update
 """
 atremis::syncsec() {
 	if [[ "$1" == "-ss" || "$1" == "--syncsec" ]]; then
 		[ $# -lt 2 ] && tiang::error "1 parameter is required for --syncsec"
 		tiang::runParallelOnTargets tiang::runSCP "../secrets/$2" "/srv/secrets/$2"
 		tiangHandledParams=2
+	elif [[ "$1" == "-pull" ]]; then
+		tiang::runParallelOnTargets tiang::runSSH atre pull
+		tiangHandledParams=1
+	elif [[ "$1" == "-upd" ]]; then
+		tiang::runParallelOnTargets tiang::runSSH atre update
+		tiangHandledParams=1
 	else
 		tiangHandledParams=0
 	fi
