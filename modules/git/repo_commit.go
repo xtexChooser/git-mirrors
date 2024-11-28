893c2f2d90b3d599e0f05402bd5809845ac89ce0
FermeLeLundi <fermelelundi@noreply.codeberg.org>
2024-11-28 10:31:28 +0000

    Update modules/git/repo_commit.go
    Typo

--- modules/git/repo_commit.go
+++ modules/git/repo_commit.go
@@ -23,8 +23,8 @@ func (repo *Repository) GetBranchCommitID(name string) (string, error) {
 }
 
 // GetTagCommitID returns last commit ID string of given tag. If the tag is an
-// annoted tag it will return the objectID of that tag instead of the commitID
-// the tag is pointing to. `GetTagCommit` handles annoted tags correctly.
+// annotated tag it will return the objectID of that tag instead of the commitID
+// the tag is pointing to. `GetTagCommit` handles annotated tags correctly.
 func (repo *Repository) GetTagCommitID(name string) (string, error) {
 	return repo.GetRefCommitID(TagPrefix + name)
 }
