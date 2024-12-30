15ff226a5b609f3ee7de60c931ed4f86d79f4cb3
Lunny Xiao <xiaolunwen@gmail.com>
2024-12-30 14:35:46 -0800

    Fix issue comment number (#30556)
    (cherry picked from commit a54cc05d2a19262a8d59f2b90fbcdd9ee5199ca8)
    
    Conflicts:
    	models/issues/comment_test.go
      trivial context conflicts

--- models/repo_test.go
+++ models/repo_test.go
@@ -7,8 +7,10 @@ import (
 	"testing"
 
 	"code.gitea.io/gitea/models/db"
+	issues_model "code.gitea.io/gitea/models/issues"
 	"code.gitea.io/gitea/models/unittest"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,4 +23,17 @@ func TestDoctorUserStarNum(t *testing.T) {
 	require.NoError(t, unittest.PrepareTestDatabase())
 
 	require.NoError(t, DoctorUserStarNum(db.DefaultContext))
+}
+
+func Test_repoStatsCorrectIssueNumComments(t *testing.T) {
+	require.NoError(t, unittest.PrepareTestDatabase())
+
+	issue2 := unittest.AssertExistsAndLoadBean(t, &issues_model.Issue{ID: 2})
+	assert.NotNil(t, issue2)
+	assert.EqualValues(t, 0, issue2.NumComments) // the fixture data is wrong, but we don't fix it here
+
+	require.NoError(t, repoStatsCorrectIssueNumComments(db.DefaultContext, 2))
+	// reload the issue
+	issue2 = unittest.AssertExistsAndLoadBean(t, &issues_model.Issue{ID: 2})
+	assert.EqualValues(t, 1, issue2.NumComments)
 }
