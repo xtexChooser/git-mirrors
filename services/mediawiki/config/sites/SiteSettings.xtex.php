xtexChooser/git-mirrors

commit c60f864ae66086063efa345407bf173bdf997af4

MW: xtex: Add xtex's K wiki
Change-Id: I243c870249021d4864fe5da23102a045bec2406a
Bug: #39

services/mediawiki/config/sites/SiteSettings.xtex.php
@@ -1,0 +1,52 @@
+<?php
+$wgSitename = "xtex's K";
+$wgMetaNamespace = "K";
+
+$xvTesting = true;
+$xvCentralized = true;
+
+$xvUseLockdown = true;
+
+require_once "$xvConfigDirectory/common/LoadExtensions.php";
+
+$wgLanguageCode = 'zh';
+$wgLocaltimezone = 'Asia/Shanghai';
+
+$wgRightsUrl = "https://creativecommons.org/licenses/by-sa/4.0/";
+$wgRightsText = "CC BY-SA 4.0";
+$wgRightsIcon = "$wgResourceBasePath/resources/assets/licenses/cc-by-sa.png";
+
+xvLoadSkin('Lakeus');
+$wgDefaultSkin = $wgDefaultMobileSkin = 'lakeus';
+
+$xvUseEmailConfirmed = true;
+
+// User rights
+$xvRequireEmailConfirmedToEdit = true;
+$wgAddGroups['l-accessor'] = [];
+$wgAddGroups['m-accessor'] = ['l-accessor'];
+
+// Namespaces
+const NS_L = 3000;
+const NS_L_TALK = 3001;
+$wgExtraNamespaces[NS_L] = 'L';
+$wgExtraNamespaces[NS_L_TALK] = 'L讨论';
+$wgContentNamespaces[] = NS_L;
+xvSetAssocTrues('wgNamespacesWithSubpages', [NS_L, NS_L_TALK]);
+xvMergeInto('wgNonincludableNamespaces', [NS_L, NS_L_TALK]);
+$wgVisualEditorAvailableNamespaces['L'] = true;
+
+const NS_M = 3002;
+const NS_M_TALK = 3003;
+$wgExtraNamespaces[NS_L] = 'M';
+$wgExtraNamespaces[NS_L_TALK] = 'M讨论';
+$wgContentNamespaces[] = NS_M;
+xvSetAssocTrues('wgNamespacesWithSubpages', [NS_M, NS_L_TALK]);
+xvMergeInto('wgNonincludableNamespaces', [NS_M, NS_L_TALK]);
+$wgVisualEditorAvailableNamespaces['M'] = true;
+
+// Lockdown
+$wgNamespacePermissionLockdown[NS_L]['read'] = ['l-accessor'];
+$wgNamespacePermissionLockdown[NS_L]['edit'] = ['l-accessor'];
+$wgNamespacePermissionLockdown[NS_M]['read'] = ['m-accessor'];
+$wgNamespacePermissionLockdown[NS_M]['edit'] = ['m-accessor'];

commit 98c645f55335fd0650b4d885e21801e2409e7414

MW: xtex: Apply GlobalSettings
Change-Id: I81f36e3a81dc36e2ddc659ded68320f8ad4123e8
Bug: #39
Fixes: c60f864ae660 ("MW: xtex: Add xtex's K wiki")

services/mediawiki/config/sites/SiteSettings.xtex.php
@@ -51,3 +51,5 @@ $wgNamespacePermissionLockdown[NS_L]['read'] = ['l-accessor'];
 $wgNamespacePermissionLockdown[NS_L]['edit'] = ['l-accessor'];
 $wgNamespacePermissionLockdown[NS_M]['read'] = ['m-accessor'];
 $wgNamespacePermissionLockdown[NS_M]['edit'] = ['m-accessor'];
+
+require_once "$xvConfigDirectory/common/GlobalSettings.php";