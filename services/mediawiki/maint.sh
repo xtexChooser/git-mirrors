xtexChooser/git-mirrors

commit cdecc7889455f36c1c3abe00ca004169090f0a30

MW: Rewrite maintenance scripts in maint.sh

services/mediawiki/maint.sh
@@ -1,0 +1,98 @@
+# shellcheck shell=bash
+
+atre::mediawiki::maint() {
+	if [[ $# -lt 2 ]]; then
+		echo "usage: atre s mediawiki maint <WIKI> <SCRIPT> [ARGUMENTS]" >&2
+		return 1
+	fi
+	local wiki="$1" script="$2"
+	shift 2
+	podman exec -it mediawiki php maintenance/run.php "$script" --wiki "$wiki" "$@"
+	return
+}
+
+atre::mediawiki::shell() {
+	atre::mediawiki::maint "$1" shell
+	return
+}
+
+atre::mediawiki::update() {
+	atre::publog "MW: Running update: $1" \
+		"MediaWiki-Wiki: $1"
+	atre::mediawiki::maint "$1" update --quick
+	return
+}
+
+atre::mediawiki::delwiki() {
+	(($# != 1)) && {
+		echo "usage: atre s mediawiki delwiki <WIKI ID>" >&2
+		return 1
+	}
+	echo "Deleting wiki: $1"
+	atre::publog "MW: Deleting wiki: $1" \
+		"MediaWiki-Wiki: $1"
+	atre::mediawiki::maint meta sql --query "DROP DATABASE wiki$1;"
+	atre::publog "MW: Deleted wiki: $1" \
+		"MediaWiki-Wiki: $1"
+	echo "Deleted wiki: $1"
+	return
+}
+
+atre::mediawiki::addwiki() {
+	(($# != 1)) && {
+		echo "usage: atre s mediawiki addwiki <WIKI ID>" >&2
+		return 1
+	}
+	local wiki="$1" domain
+	domain="$(jq -r ".$wiki" /srv/atremis/services/mediawiki/config/sites.json)"
+	echo "Found domain: $domain"
+	[[ -n "$wiki" ]] || atre::error "Invalid wiki ID"
+	[[ -n "$domain" ]] || atre::error "Invalid wiki domain"
+	[[ -e /srv/atremis/services/mediawiki/config/sites/SiteSettings."$wiki".php ]] || atre::error "Wiki site settings not found"
+
+	echo "Creating wiki $wiki, at $domain"
+	atre::publog "MW: Creating wiki: $wiki" \
+		"MediaWiki-Wiki: $wiki"
+
+	if [[ "$wiki" == "meta" ]]; then
+		# Bootstraping the meta wiki
+		atre s mariadb/monto sql -u mediawikiadmin --password -e 'CREATE DATABASE wikimeta;'
+		atre::mediawiki::maint "$wiki" sql maintenance/tables-generated.sql
+		atre::mediawiki::maint "$wiki" sql maintenance/tables.sql
+		atre::mediawiki::update "$wiki"
+	else
+		# Creating normal wikis
+		atre::mediawiki::maint meta sql --query "CREATE DATABASE wiki$wiki"
+		atre::mediawiki::maint "$wiki" sql maintenance/tables-generated.sql
+		atre::mediawiki::maint "$wiki" sql maintenance/tables.sql
+		atre::mediawiki::update "$wiki"
+	fi
+
+	atre::mediawiki::maint meta addSite wiki"$wiki" xvwiki \
+		--pagepath "https://$domain/w/\$1" \
+		--filepath "https://$domain/\$1"
+
+	atre::publog "MW: Created wiki: $wiki" \
+		"MediaWiki-Wiki: $wiki"
+	echo "Wiki $1 is created"
+}
+
+atre::mediawiki::addcargo() {
+	(($# != 1)) && {
+		echo "usage: atre s mediawiki addcargo <WIKI ID>" >&2
+		return 1
+	}
+	local wiki="$1"
+	[[ -n "$wiki" ]] || atre::error "Invalid wiki ID"
+	[[ -e /srv/atremis/services/mediawiki/config/sites/SiteSettings."$wiki".php ]] || atre::error "Wiki site settings not found"
+
+	echo "Creating cargo database $wiki, at $domain"
+	atre::publog "MW: Creating cargo database for $wiki" \
+		"MediaWiki-Wiki: $wiki"
+
+	atre::mediawiki::maint meta sql --query "CREATE DATABASE wikicargo$wiki"
+
+	atre::publog "MW: Created cargo database for $wiki" \
+		"MediaWiki-Wiki: $wiki"
+	echo "Wiki cargo database created for $1"
+}

services/mediawiki/scripts/maint.sh
@@ -1,7 +1,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-wiki=$1
-shift
-exec podman exec -it mediawiki php maintenance/run.php --wiki "$wiki" "$@"

services/mediawiki/scripts/createcargo.sh
@@ -1,17 +1,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-runMW() {
-	podman exec -it mediawiki "$@"
-}
-
-(($# != 1)) && {
-	echo "Usage: atre s mediawiki createcargo <WIKI ID>" >&2
-	exit 1
-}
-echo "Creating cargo database for wiki $1"
-
-runMW php maintenance/sql.php --wiki "$1" --query "CREATE DATABASE wikicargo$1"
-
-echo "Cargo database for wiki $1 initialized"

services/mediawiki/scripts/createwiki.sh
@@ -1,37 +1,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-runMW() {
-	podman exec -it mediawiki "$@"
-}
-
-(($# != 2)) && {
-	echo "Usage: atre s mediawiki createwiki <WIKI ID> <DOMAIN>" >&2
-	exit 1
-}
-domain="$(jq -r ".$1" /srv/atremis/services/mediawiki/config/sites.json)"
-echo "Creating wiki $1 at $domain"
-echo "---------- To revert changes: sudo podman exec -it mediawiki php maintenance/sql.php --wiki \"meta\" --query \"DROP DATABASE wiki$1;\""
-[[ -n "$domain" ]] || exit 1
-[[ -e /srv/atremis/services/mediawiki/config/sites/SiteSettings."$1".php ]] || exit 1
-
-if [[ "$1" != "meta" ]]; then
-	runMW php maintenance/sql.php --wiki "meta" --query "CREATE DATABASE wiki$1"
-	runMW php maintenance/sql.php --wiki "$1" --noshared maintenance/tables-generated.sql
-	runMW php maintenance/sql.php --wiki "$1" --noshared maintenance/tables.sql
-	runMW php maintenance/run.php --wiki "$1" update --quick
-else
-	# Bootstraping the meta wiki
-	/srv/atremis/services/mariadb/monto/script/sql.sh -u mediawikiadmin --password -e 'CREATE DATABASE wikimeta;'
-	runMW php maintenance/sql.php --wiki "$1" maintenance/tables-generated.sql
-	runMW php maintenance/sql.php --wiki "$1" maintenance/tables.sql
-	runMW php maintenance/run.php --wiki "$1" update --quick
-fi
-
-# this is broken
-# runMW php maintenance/run.php --wiki "meta" addSite wiki"$1" xvwiki \
-# 	--pagepath "https://$domain/w/\$1" \
-# 	--filepath "https://$domain/\$1"
-
-echo "Wiki $1 initialized"

services/mediawiki/scripts/shell.sh
@@ -1,7 +1,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-wiki=$1
-shift
-exec podman exec -it mediawiki php maintenance/run.php shell --wiki "$wiki"

services/mediawiki/scripts/update.sh
@@ -1,7 +1,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-wiki=$1
-shift
-exec podman exec -it mediawiki php maintenance/run.php update --wiki "$wiki" --quick

commit d57258c34a2dbd0664cd5beb699784a5701f88d7

MW: Import table schemas for normal wikis without touching shared tables
Change-Id: I7297f6ebcb701d0c95ead8732f11d1ddc0923368
Bug: #39
Fixes: cdecc7889455 ("MW: Rewrite maintenance scripts in maint.sh")

services/mediawiki/maint.sh
@@ -76,8 +76,8 @@ atre::mediawiki::addwiki() {
 	else
 		# Creating normal wikis
 		atre::mediawiki::maint meta sql --query "CREATE DATABASE wiki$wiki"
-		atre::mediawiki::maint "$wiki" sql maintenance/tables-generated.sql
-		atre::mediawiki::maint "$wiki" sql maintenance/tables.sql
+		atre::mediawiki::maint "$wiki" sql --noshared maintenance/tables-generated.sql
+		atre::mediawiki::maint "$wiki" sql --noshared maintenance/tables.sql
 		atre::mediawiki::update "$wiki"
 	fi
 