299d4bed43c5df67ab407a57b48678828a04d878
xtex <xtexchooser@duck.com>
2025-01-05 14:11:41 +0800

    MW: Un-mount mediawiki image from caddy container
    Change-Id: I0070c9c34f5f7d0262a38e42526282e86e4deb36
    Bug: #69

--- services/mediawiki/hooks/caddy-container-opts.mk
+++ services/mediawiki/hooks/caddy-container-opts.mk
@@ -1,3 +1,1 @@
-V_ARGS		+= --mount=type=image,source=codeberg.org/xens/x-mediawiki:latest,destination=/opt/mediawiki
 V_ARGS		+= --mount=type=bind,src=/var/lib/mediawiki,dst=/var/lib/mediawiki,ro=true
-V_ARGS		+= --label=org.eu.xvnet.x.depimgs=codeberg.org/xens/x-mediawiki:latest
