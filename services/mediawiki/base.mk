xtexChooser/git-mirrors

commit 59abe3ed89de50034028256dae10f37d3e3a24d2

MW: Bind /var/log/atremis into /var/log/mediawiki
Change-Id: I6104d0a28e364ad1d98920e634f3c4d17f5c3201
Bug: #69

services/mediawiki/base.mk
@@ -11,7 +11,7 @@ V_ARGS		+= --mount=type=bind,src=/srv/atremis/services/mediawiki/config,dst=/etc
 V_ARGS		+= --mount=type=bind,src=/srv/secrets/mw,dst=/srv/secrets/mw,ro=true
 V_ARGS		+= --mount=type=bind,src=/var/run/mediawiki,dst=/var/run/mediawiki
 V_ARGS		+= --mount=type=bind,src=/var/lib/mediawiki,dst=/var/lib/mediawiki
-V_ARGS		+= --mount=type=bind,src=/var/log/atremis,dst=/var/log/atremis
+V_ARGS		+= --mount=type=bind,src=/var/log/mediawiki,dst=/var/log/atremis
 V_ARGS		+= --mount=type=image,source=codeberg.org/xens/x-mediawiki:latest,destination=/opt/mediawiki
 V_ARGS		+= --label=org.eu.xvnet.x.depimgs=codeberg.org/xens/x-mediawiki:latest
 V_ARGS		+= --memory=128M

commit c746dc5cc11add073e15d06100b06034d2012eb8

MW: Fix bind mount of log directory
Change-Id: Ide7c9dbb3d32874ea9114e0a787dfa4384e91345
Bug: #69
Fixes: 59abe3ed89de ("MW: Bind /var/log/atremis into /var/log/mediawiki")

services/mediawiki/base.mk
@@ -11,7 +11,7 @@ V_ARGS		+= --mount=type=bind,src=/srv/atremis/services/mediawiki/config,dst=/etc
 V_ARGS		+= --mount=type=bind,src=/srv/secrets/mw,dst=/srv/secrets/mw,ro=true
 V_ARGS		+= --mount=type=bind,src=/var/run/mediawiki,dst=/var/run/mediawiki
 V_ARGS		+= --mount=type=bind,src=/var/lib/mediawiki,dst=/var/lib/mediawiki
-V_ARGS		+= --mount=type=bind,src=/var/log/mediawiki,dst=/var/log/atremis
+V_ARGS		+= --mount=type=bind,src=/var/log/atremis,dst=/var/log/mediawiki
 V_ARGS		+= --mount=type=image,source=codeberg.org/xens/x-mediawiki:latest,destination=/opt/mediawiki
 V_ARGS		+= --label=org.eu.xvnet.x.depimgs=codeberg.org/xens/x-mediawiki:latest
 V_ARGS		+= --memory=128M