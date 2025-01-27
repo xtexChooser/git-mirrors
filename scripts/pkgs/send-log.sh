14e98f3bdeed4695b0ef3b2c776dbe1daf8f8231
xtex <xtexchooser@duck.com>
2025-01-27 17:06:34 +0800

    CI: Remove deprecated environment variables
    Removed by Woodpecker
    
    Change-Id: I5638ec08a5ebea76c0e4bf581d11984377d05279

--- scripts/pkgs/send-log.sh
+++ scripts/pkgs/send-log.sh
@@ -4,7 +4,6 @@ set -e
 command -v curl >/dev/null 2>&1 || apk add curl
 
 [[ "${CI_WORKFLOW_NAME:-}" == "" ]] && echo "CI_WORKFLOW_NAME is not available" && exit 1
-[[ "${CI_PIPELINE_STATUS:-}" == "" ]] && echo "CI_PIPELINE_STATUS is not available" && exit 1
 [[ "${CI_PIPELINE_NUMBER:-}" == "" ]] && echo "CI_PIPELINE_NUMBER is not available" && exit 1
 [[ "${CI_PIPELINE_EVENT:-}" == "" ]] && echo "CI_PIPELINE_EVENT is not available" && exit 1
 [[ "${CI_STEP_URL:-}" == "" ]] && echo "CI_STEP_URL is not available" && exit 1
@@ -15,27 +14,16 @@ if [[ "$VERSION" != "" ]]; then
 	pipeline="$pipeline $VERSION"
 fi
 
+title="$pipeline build ended"
 desc="CI-Pipeline-Number: $CI_PIPELINE_NUMBER
 CI-Link: <$CI_STEP_URL>
 Triggerred-by-event: $CI_PIPELINE_EVENT"
-priority=min
-
-if [[ "$CI_PIPELINE_STATUS" == "success" ]]; then
-	title="$pipeline build succeeded"
-elif [[ "$CI_PIPELINE_STATUS" == "failure" ]]; then
-	title="$pipeline build failed"
-	priority=high
-else
-	title="pipelines: Pipeline status unknown ($CI_PIPELINE_STATUS): $pipeline"
-	priority=high
-fi
 
 curl \
 	-H "Authorization: Bearer $NTFY_TOKEN" \
 	-H "X-Title: $title" \
 	-H "X-Actions: view, View on CI, $CI_STEP_URL" \
 	-H "X-Tags: pipline-$pipeline,pipeline-$CI_PIPELINE_STATUS" \
-	-H "X-Priority: $priority" \
 	-d "$desc" \
 	-SL \
 	https://ntfy.xvnet.eu.org/publogs
